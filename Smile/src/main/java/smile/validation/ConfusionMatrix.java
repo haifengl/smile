c50ada4c46f9e09b25efd89311d4b5b965d026ff
Haifeng Li <haifeng.li@adp.com>
2015-05-26 22:13:24 -0400

    Fix copyrights

--- Smile/src/main/java/smile/validation/ConfusionMatrix.java
+++ Smile/src/main/java/smile/validation/ConfusionMatrix.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Haifeng Li
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package smile.validation;
 
 import java.util.HashSet;
@@ -6,24 +21,12 @@ import java.util.Set;
 /**
  * Generates the confusion matrix based on truth and prediction vectors
  * @author owlmsj
- *
  */
 public class ConfusionMatrix {
 	
 	private int[][] matrix;
 	
-	public ConfusionMatrix() {}
-	
 	public ConfusionMatrix(int[] truth, int[] prediction) {
-		generate(truth,prediction);
-	}
-	
-	public int[][] getMatrix() {
-		return matrix;
-	}
-	
-	public int[][] generate(int[] truth, int[] prediction) {
-		
 		if(truth.length != prediction.length){
 			 throw new IllegalArgumentException(String.format("The vector sizes don't match: %d != %d.", truth.length, prediction.length));
 		}
@@ -41,10 +44,12 @@ public class ConfusionMatrix {
 		}
 		
 		ySet.clear();
-		
-		return matrix;
 	}
 	
+    public int[][] getMatrix() {
+        return matrix;
+    }
+    
 	@Override
 	public String toString() {
 		
