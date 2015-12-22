9c8b7d735f3c3b4074f7b3d4913c269991b72911
Haifeng Li <haifeng.li@adp.com>
2015-12-22 13:53:58 -0500

    gradient boost benchmark on airline data

--- benchmark/src/main/scala/smile/benchmark/Benchmark.scala
+++ benchmark/src/main/scala/smile/benchmark/Benchmark.scala
@@ -17,7 +17,7 @@ package smile.benchmark
 
 import smile.data._
 import smile.data.parser.DelimitedTextParser
-import smile.classification.{LogisticRegression, NeuralNetwork, SVM, RandomForest, RBFNetwork}
+import smile.classification._
 import smile.math.Math
 import smile.math.distance.EuclideanDistance
 import smile.math.kernel.GaussianKernel
@@ -53,7 +53,7 @@ object Benchmark {
     attributes(7) = new NumericAttribute("V8")
 
     val train = parser.parse("Benchmark train", attributes, "test-data/src/main/resources/smile/data/airline/train-0.1m.csv")
-    val test = parser.parse("Benchmark Test", attributes, "test-data/src/main/resources//smile/data/airline/test.csv")
+    val test = parser.parse("Benchmark test", attributes, "test-data/src/main/resources//smile/data/airline/test.csv")
     println("class: " + train.response.asInstanceOf[NominalAttribute].values.mkString(", "))
 
     val x = train.toArray(new Array[Array[Double]](train.size))
@@ -61,24 +61,40 @@ object Benchmark {
     val testx = test.toArray(new Array[Array[Double]](test.size))
     val testy = test.toArray(new Array[Int](test.size))
 
-    val start = System.currentTimeMillis()
-    val forest = new RandomForest(x, y, 500)
-    val end = System.currentTimeMillis()
+    // Random Forest
+    var start = System.currentTimeMillis()
+    val forest = new RandomForest(attributes, x, y, 500, 3)
+    var end = System.currentTimeMillis()
     println("Random Forest 500 trees training time: %.2fs" format ((end-start)/1000.0))
 
     val posteriori = Array(0.0, 0.0)
     val prob = new Array[Double](testx.length)
-    val error = (0 until testx.length).foldLeft(0) { (e, i) =>
+    var error = (0 until testx.length).foldLeft(0) { (e, i) =>
       val yi = forest.predict(testx(i), posteriori)
       prob(i) = posteriori(1)
-      println(prob(i), testy(i))
       if (yi != testy(i)) e + 1 else e
     }
 
-    val auc = 100.0 * new AUC().measure(testy, prob)
+    var auc = 100.0 * new AUC().measure(testy, prob)
     println("Random Forest OOB error rate = %.2f%%" format (100.0 * forest.error()))
     println("Random Forest error rate = %.2f%%" format (100.0 * error / testx.length))
     println("Random Forest AUC = %.2f%%" format auc)
+
+    // Gradient Tree Boost
+    start = System.currentTimeMillis()
+    val boost = new GradientTreeBoost(attributes, x, y, 100, 1000, 0.01, 0.7)
+    end = System.currentTimeMillis()
+    println("Gradient Tree Boost 100 trees training time: %.2fs" format ((end-start)/1000.0))
+
+    error = (0 until testx.length).foldLeft(0) { (e, i) =>
+      val yi = boost.predict(testx(i), posteriori)
+      prob(i) = posteriori(1)
+      if (yi != testy(i)) e + 1 else e
+    }
+
+    auc = 100.0 * new AUC().measure(testy, prob)
+    println("Gradient Tree Boost error rate = %.2f%%" format (100.0 * error / testx.length))
+    println("Gradient Tree Boost AUC = %.2f%%" format auc)
   }
 
   def usps() {
