43741dad72d1574b1e2470f65b230e306528e302
Haifeng Li <haifeng.li@adp.com>
2016-01-12 00:29:49 -0500

    follow sidebar

---
+++ shell/src/universal/doc/js/follow-sidebar.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    var $sidebar   = $("#sidebar"),
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+});
