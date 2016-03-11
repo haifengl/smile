7814d98aef688dbaba60673a5dcd17a29de126eb
Haifeng Li <haifeng.li@adp.com>
2016-03-11 14:10:43 -0500

    iterable, rownames, colnames

--- scala/src/main/scala/smile/data/package.scala
+++ scala/src/main/scala/smile/data/package.scala
@@ -17,7 +17,7 @@
 package smile
 
 import scala.language.implicitConversions
-import smile.data._
+import scala.collection.JavaConverters._
 
 /** Data manipulation functions.
   *
@@ -25,7 +25,7 @@ import smile.data._
   */
 package object data {
 
-  implicit def pimpDataset(data: Dataset[Array[Double]]) = new PimpedDataset(data)
+  implicit def pimpDataset(data: AttributeDataset) = new PimpedDataset(data)
   implicit def pimpSparseDataset(data: SparseDataset) = new PimpedSparseDataset(data)
   implicit def pimpArray(data: Array[Double]) = new PimpedArray(data)
   implicit def pimpArray2D(data: Array[Array[Double]]) = new PimpedArray2D(data)
@@ -33,9 +33,21 @@ package object data {
 
 package data {
 
-import smile.math.Math
+import smile.math.{SparseArray, Math}
 
-private[data] class PimpedDataset(data: Dataset[Array[Double]]) {
+private[data] class PimpedDataset(data: AttributeDataset) extends Iterable[Datum[Array[Double]]] {
+  override def iterator : Iterator[Datum[Array[Double]]] = data.iterator.asScala
+
+  /** Returns the row names. */
+  def rownames: Array[String] = {
+    map(_.name).toArray
+  }
+
+  /** Returns the columns names. */
+  def colnames: Array[String] = {
+    data.attributes().map(_.getName).toArray
+  }
+
   /** Unzip the data. If the data contains a response variable, it won't be copied. */
   def unzip: Array[Array[Double]] = {
     data.toArray(new Array[Array[Double]](data.size))
@@ -56,7 +68,14 @@ private[data] class PimpedDataset(data: Dataset[Array[Double]]) {
   }
 }
 
-private[data] class PimpedSparseDataset(data: SparseDataset) {
+private[data] class PimpedSparseDataset(data: SparseDataset) extends Iterable[Datum[SparseArray]] {
+  override def iterator : Iterator[Datum[SparseArray]] = data.iterator.asScala
+
+  /** Returns the row names. */
+  def rownames: Array[String] = {
+    map(_.name).toArray
+  }
+
   /** Unzip the data. If the data contains a response variable, it won't be copied. */
   def unzip: Array[Array[Double]] = {
     data.toArray
@@ -93,7 +112,7 @@ private[data] class PimpedArray(data: Array[Double]) {
       * @return samples
       */
     def sample(n: Int): Array[Double] = {
-      val perm = (0 to n).toArray
+      val perm = (0 until data.length).toArray
       Math.permutate(perm)
       (0 until n).map{ i => data(perm(i)) }.toArray
     }
@@ -103,25 +122,29 @@ private[data] class PimpedArray(data: Array[Double]) {
       * @return samples
       */
     def sample(f: Double): Array[Double] = {
-      val n = Math.round(data.length * f).toInt
-      val perm = (0 to n).toArray
+      val perm = (0 until data.length).toArray
       Math.permutate(perm)
+      val n = Math.round(data.length * f).toInt
       (0 until n).map{ i => data(perm(i)) }.toArray
     }
   }
 
   private[data] class PimpedArray2D(data: Array[Array[Double]]) {
-    /** Get an element */
+    def nrows: Int = data.length
+
+    def ncols: Int = data(0).length
+
+    /** Returns multiple rows. */
     def apply(rows: Int*): Array[Array[Double]] = {
       rows.map { row => data(row) }.toArray
     }
 
-    /** Get a range of array */
+    /** Returns a range of rows. */
     def apply(rows: Range): Array[Array[Double]] = {
       rows.map { row => data(row) }.toArray
     }
 
-    /** Get a range of array */
+    /** Returns a submatrix. */
     def apply(rows: Range, cols: Range): Array[Array[Double]] = {
       rows.map { row =>
         val x = data(row)
@@ -129,24 +152,25 @@ private[data] class PimpedArray(data: Array[Double]) {
       }.toArray
     }
 
-    def nrows: Int = data.length
+    /** Returns a column. */
+    def ::(col: Int): Array[Double] = {
+      data.map(_(col)).toArray
+    }
 
-    def ncols: Int = data(0).length
-
-    /** Get a row of array */
+    /** Returns multiple rows. */
     def row(i: Int*): Array[Array[Double]] = apply(i: _*)
 
-    /** Get rows of array */
+    /** Returns a range of rows. */
     def row(i: Range): Array[Array[Double]] = apply(i)
 
-    /** Get a column of array */
+    /** Returns multiple columns. */
     def col(j: Int*): Array[Array[Double]] = {
       data.map { x =>
         j.map { col => x(col) }.toArray
       }.toArray
     }
 
-    /** Get columns of array */
+    /** Returns a range of columns. */
     def col(j: Range): Array[Array[Double]] = {
       data.map { x =>
         j.map { col => x(col) }.toArray
@@ -158,7 +182,7 @@ private[data] class PimpedArray(data: Array[Double]) {
       * @return samples
       */
     def sample(n: Int): Array[Array[Double]] = {
-      val perm = (0 to n).toArray
+      val perm = (0 to data.length).toArray
       Math.permutate(perm)
       (0 until n).map{ i => data(perm(i)) }.toArray
     }
@@ -168,9 +192,9 @@ private[data] class PimpedArray(data: Array[Double]) {
       * @return samples
       */
     def sample(f: Double): Array[Array[Double]] = {
-      val n = Math.round(nrows * f).toInt
-      val perm = (0 to n).toArray
+      val perm = (0 to data.length).toArray
       Math.permutate(perm)
+      val n = Math.round(nrows * f).toInt
       (0 until n).map{ i => data(perm(i)) }.toArray
     }
   }
