95a54a7e88db818bb0affca64b010fda5165dfb5
Haifeng Li <haifeng.li@adp.com>
2016-05-02 22:54:14 -0400

    run smile from stage directory

--- smile.sh
+++ smile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 sbt stage
-shell/target/universal/stage/bin/smile
+
+if [ $? -ne 0 ]; then
+  echo "sbt return code was not zero but $retval"
+  exit
+fi
+
+cd shell/target/universal/stage/
+bin/smile
+cd ../../../..
