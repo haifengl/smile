086600e30f5d885749dc8f02504c4c2b7f8d7dde
Haifeng Li <haifeng.hli@gmail.com>
2016-08-31 11:23:22 -0400

    update PCGMatrix due to the changes of IMatrix

--- core/src/main/java/smile/regression/LASSO.java
+++ core/src/main/java/smile/regression/LASSO.java
@@ -596,10 +596,15 @@ public class LASSO  implements Regression<double[]> {
         }
 
         @Override
+        public double apply(int i, int j) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+/*
+        @Override
         public PCGMatrix set(int i, int j, double x) {
             throw new UnsupportedOperationException("Not supported yet.");
         }
-
+*/
         @Override
         public void axpy(double[] x, double[] y) {
             throw new UnsupportedOperationException("Not supported yet.");
