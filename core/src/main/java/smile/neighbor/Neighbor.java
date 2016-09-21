200063a67bafd7b46fa911f88fd72c1858ffe39b
Haifeng Li <haifeng.hli@gmail.com>
2016-09-21 08:56:50 -0400

    sort neighbor by distance then by sample index

--- core/src/main/java/smile/neighbor/Neighbor.java
+++ core/src/main/java/smile/neighbor/Neighbor.java
@@ -62,6 +62,12 @@ public class Neighbor<K, V> implements Comparable<Neighbor<K,V>> {
 
     @Override
     public int compareTo(Neighbor<K,V> o) {
-        return (int) Math.signum(distance - o.distance);
+        int d = (int) Math.signum(distance - o.distance);
+        // Sometime, the dataset contains duplicate samples.
+        // If the distances are same, we sort by the sample index.
+        if (d == 0)
+            return index - o.index;
+        else
+            return d;
     }
 }
