c89e584246879c102859265f576f4eff71104d0e
Haifeng Li <haifeng.hli@gmail.com>
2017-02-01 12:51:11 -0500

    fix constructor due to name change

--- core/src/main/java/smile/wavelet/SymletWavelet.java
+++ core/src/main/java/smile/wavelet/SymletWavelet.java
@@ -104,7 +104,7 @@ public class SymletWavelet extends Wavelet {
      * Constructor. Create a Symmlet wavelet with n coefficients.
      * n = 8, 10, 12, 14, 16, 18, or 20 are supported.
      */
-    public SymmletWavelet(int n) {
+    public SymletWavelet(int n) {
         super(n == 8 ? c8 :
               n == 10 ? c10 :
               n == 12 ? c12 :
