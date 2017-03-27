17a11bf4093f4d29faecc1bc8bc5fd0ff974f215
Haifeng Li <haifeng.hli@gmail.com>
2017-03-26 22:12:30 -0400

    fix typo

--- interpolation/src/main/java/smile/interpolation/BilinearInterpolation.java
+++ interpolation/src/main/java/smile/interpolation/BilinearInterpolation.java
@@ -21,7 +21,7 @@ package smile.interpolation;
  * of linear interpolation for interpolating functions of two variables on a
  * regular grid. The key idea is to perform linear interpolation first in one
  * direction, and then again in the other direction.
-
+ *
  * @author Haifeng Li
  */
 public class BilinearInterpolation implements Interpolation2D {
