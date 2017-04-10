79820f05299f296c0ade8e9e1c457de1cc4d464a
Haifeng Li <haifeng.hli@gmail.com>
2017-04-10 14:35:30 -0400

    print out OOB size. pure vote, no tree weight

--- core/src/main/java/smile/classification/RandomForest.java
+++ core/src/main/java/smile/classification/RandomForest.java
@@ -403,7 +403,7 @@ public class RandomForest implements SoftClassifier<double[]>, Serializable {
             double accuracy = 1.0;
             if (oob != 0) {
                 accuracy = (double) correct / oob;
-                logger.info("Random forest tree OOB accuracy: {}", String.format("%.2f%%", 100 * accuracy));
+                logger.info("Random forest tree OOB size: {}, accuracy: {}", oob, String.format("%.2f%%", 100 * accuracy));
             } else {
                 logger.error("Random forest has a tree trained without OOB samples.");
             }
@@ -705,10 +705,10 @@ public class RandomForest implements SoftClassifier<double[]>, Serializable {
 
         Arrays.fill(posteriori, 0.0);
 
-        double[] y = new double[k];
+        int[] y = new int[k];
         double[] pos = new double[k];
         for (Tree tree : trees) {
-            y[tree.tree.predict(x, pos)] += tree.weight;
+            y[tree.tree.predict(x, pos)]++;
             for (int i = 0; i < k; i++) {
                 posteriori[i] += tree.weight * pos[i];
             }
