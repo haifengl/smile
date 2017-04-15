29d6d96606547b2dc5e0bd5b963be14e0a18e0a2
Haifeng Li <haifeng.hli@gmail.com>
2017-04-15 12:10:00 -0400

    improve range search check

--- core/src/main/java/smile/neighbor/KDTree.java
+++ core/src/main/java/smile/neighbor/KDTree.java
@@ -374,7 +374,7 @@ public class KDTree <E> implements NearestNeighborSearch<double[], E>, KNNSearch
             search(q, nearer, radius, neighbors);
 
             // now look in further half
-            if (radius >= diff * diff) {
+            if (radius >= Math.abs(diff)) {
                 search(q, further, radius, neighbors);
             }
         }
