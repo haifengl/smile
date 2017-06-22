f0e6930315723b6a9313cfe993fe3c550bd1c8e5
Haifeng Li <haifeng.hli@gmail.com>
2017-06-21 22:32:59 -0400

    merge CalcTest and CalculusTest

--- symbolic/src/test/java/smile/symbolic/CalculusTest.java
+++ symbolic/src/test/java/smile/symbolic/CalculusTest.java
@@ -29,7 +29,83 @@ import static org.junit.Assert.*;
  * @author Ernest DeFoy
  */
 public class CalculusTest {
+    private Calculus calculus = new Calculus();
 
+    @Test
+    void diffReadable() throws InvalidExpressionException {
+
+        final String QUERY = "0*x^2+4*1*x^(2-1)+1*x^(3-1)*cot(x^3)";
+        final String EXPECTED = "4*x+x^2*cot(x^3)";
+        String actual = Calculus.diffReadable(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    @Test
+    void reduce() throws InvalidExpressionException {
+        final String QUERY = "0*x^2+4*1*x^(2-1)+1*x^(3-1)*cot(x^3)";
+        final String EXPECTED = "4*x+x^2*cot(x^3)";
+        String actual = Calculus.rewrite(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    //rewrite SOMETHING
+    @Test
+    void reduce2() throws InvalidExpressionException {
+
+        final String QUERY = "4*5*x";
+        final String EXPECTED = "20*x";
+        String actual = Calculus.rewrite(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    // Power Rule
+    @Test
+    void diff() throws InvalidExpressionException {
+
+        final String QUERY = "4*x^2";
+        final String EXPECTED = "8*x";
+        String actual = Calculus.diff(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    // Cosecant Identity & Chain Rule
+    @Test
+    void diff2() throws InvalidExpressionException {
+
+        final String QUERY = "-csc(x^3)";
+        final String EXPECTED = "3*x^2*cot(x^3)*csc(x^3)";
+        String actual = Calculus.diff(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    // Negative Exponent
+    @Test
+    void diff4() throws InvalidExpressionException {
+
+        final String QUERY = "1 / (x+2)";
+        final String EXPECTED = "-1/(x+2)(x+2)";
+        String actual = calculus.diff(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    // Negative Exponent
+    @Test
+    void diff5() throws InvalidExpressionException {
+
+        final String QUERY = "1 / (x^2)";
+        final String EXPECTED = "-2/x^3";
+        String actual = calculus.diff(QUERY);
+
+        assertEquals(EXPECTED, actual);
+    }
+
+    /*
     @Test
     public void diffReadable() throws InvalidExpressionException {
 
@@ -61,8 +137,5 @@ public class CalculusTest {
         // Negative Exponent 2
         assertEquals(Calculus.diff("1 / (x^2)"), "-2/x^3");
     }
-
-    @Test
-    public void evaluate() {
-    }
+*/
 }
