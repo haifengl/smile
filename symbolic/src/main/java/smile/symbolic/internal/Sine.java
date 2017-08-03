793bc5888d16bb3be90fcdaca78ab05a03fbbe8c
Haifeng Li <haifeng.hli@gmail.com>
2017-08-03 16:10:44 -0400

    update symbolic module copyright header as Ernest releases the copyright

--- symbolic/src/main/java/smile/symbolic/internal/Sine.java
+++ symbolic/src/main/java/smile/symbolic/internal/Sine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Ernest DeFoy
+ * Copyright (c) 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
