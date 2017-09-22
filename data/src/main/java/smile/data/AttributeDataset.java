692c63d5cae88d7234392d6e548acd84081d826e
Haifeng Li <haifeng.hli@gmail.com>
2017-09-22 11:23:28 -0400

    add Row in summary()

--- data/src/main/java/smile/data/AttributeDataset.java
+++ data/src/main/java/smile/data/AttributeDataset.java
@@ -322,7 +322,7 @@ public class AttributeDataset extends Dataset<double[]> {
             s[3] = Math.mean(x);
             s[4] = Math.q3(x);
             s[5] = Math.max(x);
-            Datum<double[]> datum = new Datum<>(s);
+            Row datum = new Row(s);
             datum.name = attributes[i].getName();
             datum.description = attributes[i].getDescription();
             stat.add(datum);
