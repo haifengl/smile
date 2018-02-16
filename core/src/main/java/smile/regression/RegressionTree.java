5d6c93e7f7f60376719f186c3a957abbbbd492ca
ajk <adam.jakubowski@codilime.com>
2018-02-16 16:18:40 +0100

    Remove never used split return value

--- core/src/main/java/smile/regression/RegressionTree.java
+++ core/src/main/java/smile/regression/RegressionTree.java
@@ -595,7 +595,7 @@ public class RegressionTree implements Regression<double[]>, Serializable {
         /**
          * Split the node into two children nodes. Returns true if split success.
          */
-        public boolean split(PriorityQueue<TrainNode> nextSplits) {
+        public void split(PriorityQueue<TrainNode> nextSplits) {
             if (node.splitFeature < 0) {
                 throw new IllegalStateException("Split a node with invalid feature.");
             }
@@ -640,7 +640,7 @@ public class RegressionTree implements Regression<double[]>, Serializable {
                 node.splitFeature = -1;
                 node.splitValue = Double.NaN;
                 node.splitScore = 0.0;
-                return false;
+                return;
             }
 
             node.trueChild = new Node(node.trueChildOutput);
@@ -665,8 +665,6 @@ public class RegressionTree implements Regression<double[]>, Serializable {
             }
 
             importance[node.splitFeature] += node.splitScore;
-
-            return true;
         }
     }
 
