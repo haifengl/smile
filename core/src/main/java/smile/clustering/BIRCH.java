0bfbadca0f138a2897ea49d71ca019f5d8700acf
Haifeng Li <haifeng.hli@gmail.com>
2018-08-15 10:30:30 -0400

    update centroids in partition()

--- core/src/main/java/smile/clustering/BIRCH.java
+++ core/src/main/java/smile/clustering/BIRCH.java
@@ -431,8 +431,23 @@ public class BIRCH implements Clustering<double[]>, Serializable {
             HierarchicalClustering hc = new HierarchicalClustering(linkage);
 
             int[] y = hc.partition(k);
+
+            // recalculate the centroids of each cluster
+            centroids = new double[k][d];
+            int[] nc = new int[k];
             for (int i = 0; i < n; i++) {
-                leaves.get(i).y = y[i];
+                Leaf leaf = leaves.get(i);
+                int yi = y[i];
+                leaf.y = yi;
+                nc[yi] += leaf.n;
+                for (int j = 0; j < d; j++) {
+                    centroids[yi][j] += leaf.sum[j];
+                }
+            }
+            for (int i = 0; i < k; i++) {
+                for (int j = 0; j < d; j++) {
+                    centroids[i][j] /= nc[i];
+                }
             }
         } else {
             for (int i = 0; i < n; i++) {
