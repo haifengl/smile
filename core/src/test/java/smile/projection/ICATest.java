9f98c3458309407239e92911371c63b7327c84a4
rayeaster <rayeaster@hotmail.com>
2018-10-14 23:15:26 +0800

    add testset from sklearn

--- core/src/test/java/smile/projection/ICATest.java
+++ core/src/test/java/smile/projection/ICATest.java
@@ -15,6 +15,9 @@
  *******************************************************************************/
 package smile.projection;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -22,7 +25,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import smile.data.AttributeDataset;
+import smile.data.NumericAttribute;
 import smile.data.parser.ArffParser;
+import smile.data.parser.DelimitedTextParser;
 import smile.math.matrix.DenseMatrix;
 
 /**
@@ -119,8 +124,57 @@ public class ICATest {
             	}
             	System.out.print(sp.append(point).toString());
             }
+            System.out.println();  
         } catch (Exception ex) {
             System.err.println(ex);
+        }
+    }
+    
+    /**
+     * Test of learn method, of FastICA.
+     */
+    @Test
+    public void testWithMixedSignal() {
+        System.out.println("learn ICA with mixed signal...");        
+
+        DelimitedTextParser parser = new DelimitedTextParser();
+        parser.setDelimiter(",");
+        parser.setResponseIndex(new NumericAttribute("class"), 3);
+        try {
+            AttributeDataset mixed = parser.parse(smile.data.parser.IOUtils.getTestDataFile("projection/mixed-signal/test_ica_X.csv"));
+            double[][] x = mixed.toArray(new double[mixed.size()][]);
+
+            ICA fastICA = new ICA(x, 3);
+
+            double[][] p = fastICA.project(x);
+            int size = mixed.size();
+                       
+            double[] p1 = new double[size];
+            for(int i = 0;i < size;i++) {
+            	p1[i] = p[i][0];
+            }
+            
+            double[] p2 = new double[size];
+            for(int i = 0;i < size;i++) {
+            	p2[i] = p[i][1];
+            }
+
+            double[] p3 = new double[size];
+            for(int i = 0;i < size;i++) {
+            	p3[i] = p[i][2];
+            }
+            
+//            BufferedWriter bw = new BufferedWriter(new FileWriter("test_ica_reconstruct.csv"));  
+//            for(int i = 0;i < size;i++) {
+//            	StringBuilder sb = new StringBuilder();
+//            	sb.append(p1[i]).append(",").append(p2[i]).append(",").append(p3[i]);
+//            	bw.append(sb.toString());
+//            	bw.newLine();
+//            }
+//            bw.flush();
+//            bw.close();
+        }catch(Exception e) {
+        	e.printStackTrace();
         }
     }
 
