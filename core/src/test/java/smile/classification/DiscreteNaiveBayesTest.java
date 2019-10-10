24b4ff7ce622af82dfda2896645e7433f28fce6b
Haifeng Li <haifeng.hli@gmail.com>
2019-10-09 23:38:58 -0400

    add discrete naive bayes tests

---
+++ core/src/test/java/smile/classification/DiscreteNaiveBayesTest.java
@@ -0,0 +1,151 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2019 Haifeng Li
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package smile.classification;
+
+import smile.data.Movie;
+import smile.math.MathEx;
+import smile.validation.Error;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import smile.validation.CrossValidation;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Haifeng Li
+ */
+public class DiscreteNaiveBayesTest {
+
+    public DiscreteNaiveBayesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testBatchMultinomial() {
+        System.out.println("---batch Multinomial---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.MULTINOMIAL, 2, Movie.feature.length);
+            bayes.update(x, y);
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(463, error);
+    }
+
+    @Test
+    public void testOnlineMultinomial() {
+        System.out.println("---online Multinomial---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.MULTINOMIAL, 2, Movie.feature.length);
+            for (int j = 0; j < x.length; j++) {
+                bayes.update(x[j], y[j]);
+            }
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(463, error);
+    }
+
+    @Test
+    public void testBatchPolyaUrn() {
+        System.out.println("---batch PolyaUrn---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.POLYAURN, 2, Movie.feature.length);
+            bayes.update(x, y);
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(462, error);
+    }
+
+    @Test
+    public void testOnlinePolyaUrn() {
+        System.out.println("---online PolyaUrn---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.POLYAURN, 2, Movie.feature.length);
+            for (int j = 0; j < x.length; j++) {
+                bayes.update(x[j], y[j]);
+            }
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(462, error);
+    }
+
+    @Test
+    public void testBatchBernoulli() {
+        System.out.println("---batch Bernoulli---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.BERNOULLI, 2, Movie.feature.length);
+            bayes.update(x, y);
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(435, error);
+    }
+
+    @Test
+    public void testOnlineBernoulli() {
+        System.out.println("---online Bernoulli---");
+
+        MathEx.setSeed(19650218); // to get repeatable results.
+        int[] prediction = CrossValidation.classification(10, Movie.x, Movie.y, (x, y) -> {
+            DiscreteNaiveBayes bayes = new DiscreteNaiveBayes(DiscreteNaiveBayes.Model.BERNOULLI, 2, Movie.feature.length);
+            for (int j = 0; j < x.length; j++) {
+                bayes.update(x[j], y[j]);
+            }
+            return bayes;
+        });
+        int error = Error.apply(Movie.y, prediction);
+        System.out.println("Error = " + error);
+        assertEquals(435, error);
+    }
+}
