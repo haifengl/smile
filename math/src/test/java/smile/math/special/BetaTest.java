a6a4a5947b3e969842784e45d1b5178d13e2119b
Brian Wiltse <Brian.Wiltse@seeq.com>
2019-10-30 22:37:59 -0700

    Add BetaTest.java, with tests for x boundaries in regularizedIncompleteBetaFunction

--- math/src/test/java/smile/math/special/BetaTest.java
+++ math/src/test/java/smile/math/special/BetaTest.java
@@ -16,93 +16,37 @@
 
 package smile.math.special;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author Haifeng Li
- */
-public class GammaTest {
-
-    public GammaTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
+public class BetaTest {
+    /**
+     * Test that a value of x above the upper boundary throws exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testRegularizedIncompleteBetaFunctionXBeyondUpperBoundary() {
+        Beta.regularizedIncompleteBetaFunction(1, 1, 1.00001);
     }
 
     /**
-     * Test of gamma method, of class Gamma.
+     * Test that a value of x below the lower boundary throws exception
      */
-    @Test
-    public void testGamma() {
-        System.out.println("gamma");
-        assertTrue(Double.isInfinite(Gamma.gamma(0)));
-
-        assertEquals(1.0, Gamma.gamma(1), 1E-7);
-        assertEquals(1.0, Gamma.gamma(2), 1E-7);
-        assertEquals(2.0, Gamma.gamma(3), 1E-7);
-        assertEquals(6.0, Gamma.gamma(4), 1E-7);
-
-        assertEquals(0.886227, Gamma.gamma(1.5), 1E-6);
-        assertEquals(1.329340, Gamma.gamma(2.5), 1E-6);
-        assertEquals(3.323351, Gamma.gamma(3.5), 1E-6);
-        assertEquals(11.63173, Gamma.gamma(4.5), 1E-5);
+    @Test(expected = IllegalArgumentException.class)
+    public void testRegularizedIncompleteBetaFunctionXBeyondLowerBoundary() {
+        Beta.regularizedIncompleteBetaFunction(1, 1, -0.00001);
     }
 
     /**
-     * Test of lgamma method, of class Gamma.
+     * Test that values of x within a reasonable distance from 0 and 1 are treated as 0 and 1,
+     * to account for floating point errors.
      */
     @Test
-    public void testLogGamma() {
-        System.out.println("lgamma");
-        assertTrue(Double.isInfinite(Gamma.lgamma(0)));
+    public void testRegularizedIncompleteBetaFunctionXWithinBoundaries() {
+        double pHigh = Beta.regularizedIncompleteBetaFunction(1, 1, 1.000000000000001);
+        assertEquals(1.0, pHigh, 0.0);
 
-        assertEquals(0.0, Gamma.lgamma(1), 1E-7);
-        assertEquals(0, Gamma.lgamma(2), 1E-7);
-        assertEquals(Math.log(2.0), Gamma.lgamma(3), 1E-7);
-        assertEquals(Math.log(6.0), Gamma.lgamma(4), 1E-7);
-
-        assertEquals(-0.1207822, Gamma.lgamma(1.5), 1E-7);
-        assertEquals(0.2846829, Gamma.lgamma(2.5), 1E-7);
-        assertEquals(1.200974, Gamma.lgamma(3.5), 1E-6);
-        assertEquals(2.453737, Gamma.lgamma(4.5), 1E-6);
-    }
-
-    /**
-     * Test of incompleteGamma method, of class Gamma.
-     */
-    @Test
-    public void testIncompleteGamma() {
-        System.out.println("incompleteGamma");
-        assertEquals(0.7807, Gamma.regularizedIncompleteGamma(2.1, 3), 1E-4);
-        assertEquals(0.3504, Gamma.regularizedIncompleteGamma(3, 2.1), 1E-4);
-    }
-
-    /**
-     * Test of upperIncompleteGamma method, of class Gamma.
-     */
-    @Test
-    public void testUpperIncompleteGamma() {
-        System.out.println("incompleteGamma");
-        assertEquals(0.2193, Gamma.regularizedUpperIncompleteGamma(2.1, 3), 1E-4);
-        assertEquals(0.6496, Gamma.regularizedUpperIncompleteGamma(3, 2.1), 1E-4);
+        double pLow = Beta.regularizedIncompleteBetaFunction(1, 1, -0.000000000000001);
+        assertEquals(0.0, pLow, 0.0);
     }
 }
