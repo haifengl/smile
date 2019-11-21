31502ac6b50039164000afe071797e87f0350d39
Haifeng Li <haifeng.hli@gmail.com>
2019-11-21 16:38:24 -0500

    s2n and ssr on DataFrame

--- scala/src/main/scala/smile/feature/package.scala
+++ scala/src/main/scala/smile/feature/package.scala
@@ -17,6 +17,9 @@
 
 package smile
 
+import smile.data.DataFrame
+import smile.data.formula.Formula
+
 /** Feature generation, normalization and selection.
   *
   * Feature generation (or constructive induction) studies methods that modify
@@ -64,6 +67,23 @@ package object feature {
     */
   def s2n(x: Array[Array[Double]], y: Array[Int]): Array[Double] = SignalNoiseRatio.of(x, y)
 
+  /** The signal-to-noise (S2N) metric ratio is a univariate feature ranking metric,
+    * which can be used as a feature selection criterion for binary classification
+    * problems. S2N is defined as |&mu;<sub>1</sub> - &mu;<sub>2</sub>| / (&sigma;<sub>1</sub> + &sigma;<sub>2</sub>),
+    * where &mu;<sub>1</sub> and &mu;<sub>2</sub> are the mean value of the variable
+    * in classes 1 and 2, respectively, and &sigma;<sub>1</sub> and &sigma;<sub>2</sub>
+    * are the standard deviations of the variable in classes 1 and 2, respectively.
+    * Clearly, features with larger S2N ratios are better for classification.
+    *
+    * ====References:====
+    *  - M. Shipp, et al. Diffuse large B-cell lymphoma outcome prediction by gene-expression profiling and supervised machine learning. Nature Medicine, 2002.
+    */
+  def s2n(formula: Formula, data: DataFrame): Array[Double] = {
+    val x = formula.x(data).toArray
+    val y = formula.y(data).toIntArray
+    SignalNoiseRatio.of(x, y)
+  }
+
   /** The ratio of between-groups to within-groups sum of squares is a univariate
     * feature ranking metric, which can be used as a feature selection criterion
     * for multi-class classification problems. For each variable j, this ratio is
@@ -77,4 +97,22 @@ package object feature {
     *  - S. Dudoit, J. Fridlyand and T. Speed. Comparison of discrimination methods for the classification of tumors using gene expression data. J Am Stat Assoc, 97:77-87, 2002.
     */
   def ssr(x: Array[Array[Double]], y: Array[Int]): Array[Double] = SumSquaresRatio.of(x, y)
+
+  /** The ratio of between-groups to within-groups sum of squares is a univariate
+    * feature ranking metric, which can be used as a feature selection criterion
+    * for multi-class classification problems. For each variable j, this ratio is
+    * BSS(j) / WSS(j) = &Sigma;I(y<sub>i</sub> = k)(x<sub>kj</sub> - x<sub>&middot;j</sub>)<sup>2</sup> / &Sigma;I(y<sub>i</sub> = k)(x<sub>ij</sub> - x<sub>kj</sub>)<sup>2</sup>;
+    * where x<sub>&middot;j</sub> denotes the average of variable j across all
+    * samples, x<sub>kj</sub> denotes the average of variable j across samples
+    * belonging to class k, and x<sub>ij</sub> is the value of variable j of sample i.
+    * Clearly, features with larger sum squares ratios are better for classification.
+    *
+    * ====References:====
+    *  - S. Dudoit, J. Fridlyand and T. Speed. Comparison of discrimination methods for the classification of tumors using gene expression data. J Am Stat Assoc, 97:77-87, 2002.
+    */
+  def ssr(formula: Formula, data: DataFrame): Array[Double] = {
+    val x = formula.x(data).toArray
+    val y = formula.y(data).toIntArray
+    SumSquaresRatio.of(x, y)
+  }
 }
