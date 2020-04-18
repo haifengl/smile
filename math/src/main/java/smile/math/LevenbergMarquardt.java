983742add328ac2003207f68f751c7233f599248
Haifeng Li <haifeng.hli@gmail.com>
2020-04-18 15:53:20 -0400

    LevenbergMarquardt for multivariate functions

--- math/src/main/java/smile/math/LevenbergMarquardt.java
+++ math/src/main/java/smile/math/LevenbergMarquardt.java
@@ -225,4 +225,166 @@ public class LevenbergMarquardt {
 
         return new LevenbergMarquardt(pfit, f, r, ss);
     }
+
+    /**
+     * Fits the nonlinear least squares.
+     *
+     * @param func the curve function.
+     *
+     * @param x independent variables.
+     * @param y the observations.
+     * @param p the initial parameters.
+     *
+     * @return the sum of squared errors.
+     */
+    public static LevenbergMarquardt fit(DifferentiableMultivariateFunction func, double[][] x, double[] y, double[] p) {
+        return fit(func, x, y, p, 0.0001, 20);
+    }
+
+    /**
+     * Fits the nonlinear least squares.
+     *
+     * @param func the curve function. Of the input variable x, the first d
+     *             elements are hyper-parameters to be fit. The rest is the
+     *             independent variable.
+     * @param x independent variables.
+     * @param y the observations.
+     * @param p the initial parameters.
+     * @param stol the scalar tolerances on fractional improvement in sum of squares
+     * @param maxIter the maximum number of allowed iterations.
+     *
+     * @return the sum of squared errors.
+     */
+    public static LevenbergMarquardt fit(DifferentiableMultivariateFunction func, double[][] x, double[] y, double[] p, double stol, int maxIter) {
+        if (stol <= 0.0) {
+            throw new IllegalArgumentException("Invalid gradient tolerance: " + stol);
+        }
+
+        if (maxIter <= 0) {
+            throw new IllegalArgumentException("Invalid maximum number of iterations: " + maxIter);
+        }
+
+        // set up for iterations
+        int n = x.length;
+        int m = x[0].length;
+        int d = p.length;
+        double[] pbest = new double[d+m];
+        double[] pprev = new double[d+m];
+        double[] pnew = new double[d+m];
+        System.arraycopy(p, 0, pbest, 0, d);
+
+        double[] f = new double[n];
+        double[] r = new double[n];
+        double[] g = new double[d];
+        double[] gse = new double[d];
+        double[] dp = new double[d];
+        double[] chg = new double[d];
+
+        double[] norm = new double[d];
+        Arrays.fill(norm, 1.0);
+
+        DenseMatrix J = Matrix.zeros(n, d);
+
+        double epsLlast = 1;
+        double[] epstab = {0.1, 1, 1e2, 1e4, 1e6};
+
+        // iterations
+        for (int iter = 1; iter <= maxIter; iter++) {
+            System.arraycopy(pbest, 0, pprev, 0, d);
+
+            double ss = 0.0;
+            for (int i = 0; i < n; i++) {
+                System.arraycopy(x[i], 0, pprev, d, m);
+                double fi = func.g(pprev, dp);
+                r[i] = y[i] - fi;
+                ss += r[i] * r[i];
+
+                for (int j = 0; j < d; j++) {
+                    J.set(i, j, dp[j]);
+                }
+            }
+
+            double sbest = ss;
+            double sgoal = (1 - stol) * sbest;
+
+            for (int j = 0; j < d; j++) {
+                double nrm = 0.0;
+                for (int i = 0; i < n; i++) {
+                    double dpi = J.get(i, j);
+                    nrm += dpi * dpi;
+                }
+
+                if (nrm > 0) {
+                    norm[j] = 1 / Math.sqrt(nrm);
+                } else {
+                    norm[j] = 1.0;
+                }
+
+                for (int i = 0; i < n; i++) {
+                    J.mul(i, j, norm[j]);
+                }
+            }
+
+            SVD svd = J.svd();
+            double[] s = svd.getSingularValues();
+            double s2 = MathEx.dot(s, s);
+            DenseMatrix U = svd.getU();
+            DenseMatrix V = svd.getV();
+            U.atx(r, g);
+
+            for (int k = 0; k < epstab.length; k++) {
+                double epsL = Math.max(epsLlast * epstab[k], 1e-7);
+                double se = Math.sqrt(s2 + epsL);
+                for (int j = 0; j < d; j++) {
+                    gse[j] = g[j] / se;
+                }
+
+                V.ax(gse, chg);
+
+                for (int j = 0; j < d; j++) {
+                    chg[j] *= norm[j];
+                }
+
+                for (int i = 0; i < d; i++) {
+                    pnew[i] = chg[i] + pprev[i];
+                }
+
+                ss = 0.0;
+                for (int i = 0; i < n; i++) {
+                    System.arraycopy(x[i], 0, pnew, d, m);
+                    double fi = func.f(pnew);
+                    double ri = y[i] - fi;
+                    ss += ri * ri;
+                }
+
+                if (ss < sbest) {
+                    System.arraycopy(pnew, 0, pbest, 0, d);
+                    sbest = ss;
+                }
+
+                if (ss <= sgoal) {
+                    epsLlast = epsL;
+                    break;
+                }
+            }
+
+            logger.info(String.format("SSE after %3d iterations: %.5f", iter, sbest));
+            if (ss < MathEx.EPSILON || ss > sgoal) {
+                logger.info(String.format("converges on SSE after %d iterations", iter));
+                break;
+            }
+        }
+
+        double[] pfit = new double[d];
+        System.arraycopy(pbest, 0, pfit, 0, d);
+        double ss = 0.0;
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(x[i], 0, pbest, d, m);
+            f[i] = func.f(pbest);
+            r[i] = y[i] - f[i];
+            ss += r[i] * r[i];
+        }
+
+        return new LevenbergMarquardt(pfit, f, r, ss);
+    }
 }
