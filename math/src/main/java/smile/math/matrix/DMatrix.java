c2593d4f1c6d41e93ef3d49f7ad634af5ad45f28
Haifeng Li <haifeng.hli@gmail.com>
2020-06-30 18:59:33 -0400

    restore matrix market file parser

--- math/src/main/java/smile/math/matrix/DMatrix.java
+++ math/src/main/java/smile/math/matrix/DMatrix.java
@@ -17,8 +17,19 @@
 
 package smile.math.matrix;
 
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import smile.util.SparseArray;
 import smile.math.blas.Transpose;
 import static smile.math.blas.Transpose.*;
+import static smile.math.blas.UPLO.*;
 
 /**
  * Double precision matrix base class.
@@ -212,6 +223,188 @@ public abstract class DMatrix extends IMatrix<double[]> {
             }
 
             return new Matrix.SVD(s, U, V);
+        }
+    }
+
+    /**
+     * Reads a matrix from a Matrix Market File Format file.
+     * For details, see
+     * <a href="http://people.sc.fsu.edu/~jburkardt/data/mm/mm.html">http://people.sc.fsu.edu/~jburkardt/data/mm/mm.html</a>.
+     *
+     * The returned matrix may be dense or sparse.
+     *
+     * @param path the input file path.
+     * @return a dense or sparse matrix.
+     * @author Haifeng Li
+     */
+    static DMatrix market(Path path) throws IOException, ParseException {
+        try (LineNumberReader reader = new LineNumberReader(Files.newBufferedReader(path));
+             Scanner scanner = new Scanner(reader)) {
+
+            // The header line has the form
+            // %%MatrixMarket object format field symmetry
+            String header = scanner.next();
+            if (!header.equals("%%MatrixMarket")) {
+                throw new ParseException("Invalid Matrix Market file header", reader.getLineNumber());
+            }
+
+            String object = scanner.next();
+            if (!object.equals("matrix")) {
+                throw new UnsupportedOperationException("The object is not a matrix file: " + object);
+            }
+
+            String format = scanner.next();
+            String field = scanner.next();
+            if (field.equals("complex") || field.equals("pattern")) {
+                throw new UnsupportedOperationException("No support of complex or pattern matrix");
+            }
+
+            String symmetry = scanner.nextLine().trim();
+            if (symmetry.equals("Hermitian")) {
+                throw new UnsupportedOperationException("No support of Hermitian matrix");
+            }
+
+            boolean symmetric = symmetry.equals("symmetric");
+            boolean skew = symmetry.equals("skew-symmetric");
+
+            // Ignore comment lines
+            String line = scanner.nextLine();
+            while (line.startsWith("%")) {
+                line = scanner.nextLine();
+            }
+
+            if (format.equals("array")) {
+                // Size line
+                Scanner s = new Scanner(line);
+                int nrows = s.nextInt();
+                int ncols = s.nextInt();
+
+                Matrix matrix = new Matrix(nrows, ncols);
+                for (int j = 0; j < ncols; j++) {
+                    for (int i = 0; i < nrows; i++) {
+                        double x = scanner.nextDouble();
+                        matrix.set(i, j, x);
+                    }
+                }
+
+                if (symmetric) {
+                    matrix.uplo(LOWER);
+                }
+
+                return matrix;
+            }
+
+            if (format.equals("coordinate")) {
+                // Size line
+                Scanner s = new Scanner(line);
+                int nrows = s.nextInt();
+                int ncols = s.nextInt();
+                int nz = s.nextInt();
+
+                if (symmetric && nz == nrows * (nrows + 1) / 2) {
+                    if (nrows != ncols) {
+                        throw new IllegalStateException(String.format("Symmetric matrix is not square: %d != %d", nrows, ncols));
+                    }
+
+                    SymmMatrix matrix = new SymmMatrix(LOWER, nrows);
+                    for (int k = 0; k < nz; k++) {
+                        String[] tokens = scanner.nextLine().trim().split("\\s+");
+                        if (tokens.length != 3) {
+                            throw new ParseException("Invalid data line: " + line, reader.getLineNumber());
+                        }
+
+                        int i = Integer.parseInt(tokens[0]) - 1;
+                        int j = Integer.parseInt(tokens[1]) - 1;
+                        double x = Double.parseDouble(tokens[2]);
+
+                        matrix.set(i, j, x);
+                    }
+
+                    return matrix;
+                } else if (skew && nz == nrows * (nrows + 1) / 2) {
+                    if (nrows != ncols) {
+                        throw new IllegalStateException(String.format("Skew-symmetric matrix is not square: %d != %d", nrows, ncols));
+                    }
+
+                    Matrix matrix = new Matrix(nrows, ncols);
+                    for (int k = 0; k < nz; k++) {
+                        String[] tokens = scanner.nextLine().trim().split("\\s+");
+                        if (tokens.length != 3) {
+                            throw new ParseException("Invalid data line: " + line, reader.getLineNumber());
+                        }
+
+                        int i = Integer.parseInt(tokens[0]) - 1;
+                        int j = Integer.parseInt(tokens[1]) - 1;
+                        double x = Double.parseDouble(tokens[2]);
+
+                        matrix.set(i, j, x);
+                        matrix.set(j, i, -x);
+                    }
+
+                    return matrix;
+                }
+
+                // General sparse matrix
+                int[] colSize = new int[ncols];
+                List<SparseArray> rows = new ArrayList<>();
+                for (int i = 0; i < nrows; i++) {
+                    rows.add(new SparseArray());
+                }
+
+                for (int k = 0; k < nz; k++) {
+                    String[] tokens = scanner.nextLine().trim().split("\\s+");
+                    if (tokens.length != 3) {
+                        throw new ParseException("Invalid data line: " + line, reader.getLineNumber());
+                    }
+
+                    int i = Integer.parseInt(tokens[0]) - 1;
+                    int j = Integer.parseInt(tokens[1]) - 1;
+                    double x = Double.parseDouble(tokens[2]);
+
+                    SparseArray row = rows.get(i);
+                    row.set(j, x);
+                    colSize[j] += 1;
+
+                    if (symmetric) {
+                        row = rows.get(j);
+                        row.set(i, x);
+                        colSize[i] += 1;
+                    } else if (skew) {
+                        row = rows.get(j);
+                        row.set(i, -x);
+                        colSize[i] += 1;
+                    }
+                }
+
+                int[] pos = new int[ncols];
+                int[] colIndex = new int[ncols + 1];
+                for (int i = 0; i < ncols; i++) {
+                    colIndex[i + 1] = colIndex[i] + colSize[i];
+                }
+
+                if (symmetric || skew) {
+                    nz *= 2;
+                }
+                int[] rowIndex = new int[nz];
+                double[] x = new double[nz];
+
+                for (int i = 0; i < nrows; i++) {
+                    for (SparseArray.Entry e :rows.get(i)) {
+                        int j = e.i;
+                        int k = colIndex[j] + pos[j];
+
+                        rowIndex[k] = i;
+                        x[k] = e.x;
+                        pos[j]++;
+                    }
+                }
+
+                SparseMatrix matrix = new SparseMatrix(nrows, ncols, x, rowIndex, colIndex);
+                return matrix;
+
+            }
+
+            throw new ParseException("Invalid Matrix Market format: " + format, 0);
         }
     }
 }
--- math/src/test/java/smile/math/matrix/SparseMatrixTest.java
+++ math/src/test/java/smile/math/matrix/SparseMatrixTest.java
@@ -318,4 +318,91 @@ public class SparseMatrixTest {
         assertEquals(51.0, data.get(4, 0), 1E-7);
         assertEquals(55.0, data.get(4, 4), 1E-7);
     }
+
+    @Test(expected = Test.None.class)
+    public void testMatrixMarket08blocks() throws Exception {
+        System.out.println("market 08blocks");
+        SparseMatrix data = (SparseMatrix) Matrix.market(smile.util.Paths.getTestData("matrix/08blocks.mtx"));
+        assertEquals(592, data.size());
+        assertEquals(300, data.nrows());
+        assertEquals(300, data.ncols());
+        assertEquals(94.0, data.get(36, 0), 1E-7);
+        assertEquals(1.0, data.get(0, 1), 1E-7);
+        assertEquals(33.0, data.get(36, 1), 1E-7);
+        assertEquals(95.0, data.get(299, 299), 1E-7);
+    }
+
+    @Test(expected = Test.None.class)
+    public void testMatrixMarketGr900() throws Exception {
+        System.out.println("market gr900");
+        SparseMatrix data = (SparseMatrix) Matrix.market(smile.util.Paths.getTestData("matrix/gr_900_900_crg.mm"));
+        //assertEquals(true, data.isSymmetric());
+        assertEquals(8644, data.size());
+        assertEquals(900, data.nrows());
+        assertEquals(900, data.ncols());
+        assertEquals( 8.0, data.get(0, 0), 1E-7);
+        assertEquals( 8.0, data.get(1, 1), 1E-7);
+        assertEquals(-1.0, data.get(2, 1), 1E-7);
+        assertEquals(-1.0, data.get(30, 1), 1E-7);
+        assertEquals( 8.0, data.get(899, 899), 1E-7);
+
+        // it is symmetric
+        assertEquals(-1.0, data.get(1, 2), 1E-7);
+        assertEquals(-1.0, data.get(1, 30), 1E-7);
+    }
+
+    @Test(expected = Test.None.class)
+    public void testMatrixMarketCrk() throws Exception {
+        System.out.println("market crk");
+        SparseMatrix data = (SparseMatrix) Matrix.market(smile.util.Paths.getTestData("matrix/m_05_05_crk.mm"));
+        assertEquals(8, data.size());
+        assertEquals(5, data.nrows());
+        assertEquals(5, data.ncols());
+        assertEquals( 15.0, data.get(0, 4), 1E-7);
+        assertEquals( 23.0, data.get(1, 2), 1E-7);
+        assertEquals( 24.0, data.get(1, 3), 1E-7);
+        assertEquals( 35.0, data.get(2, 4), 1E-7);
+
+        // it is skew-symmetric
+        assertEquals( -15.0, data.get(4, 0), 1E-7);
+        assertEquals( -23.0, data.get(2, 1), 1E-7);
+        assertEquals( -24.0, data.get(3, 1), 1E-7);
+        assertEquals( -35.0, data.get(4, 2), 1E-7);
+    }
+
+    /**
+     * Test of market method, of class Matrix.
+     */
+    @Test(expected = Test.None.class)
+    public void testMatrixMarketCrs() throws Exception {
+        System.out.println("market crs");
+        SparseMatrix data = (SparseMatrix) Matrix.market(smile.util.Paths.getTestData("matrix/m_05_05_crs.mm"));
+        assertEquals(18, data.size());
+        assertEquals(5, data.nrows());
+        assertEquals(5, data.ncols());
+        assertEquals(11.0, data.get(0, 0), 1E-7);
+        assertEquals(15.0, data.get(0, 4), 1E-7);
+        assertEquals(22.0, data.get(1, 1), 1E-7);
+        assertEquals(23.0, data.get(1, 2), 1E-7);
+        assertEquals(55.0, data.get(4, 4), 1E-7);
+
+        // it is skew-symmetric
+        assertEquals(15.0, data.get(4, 0), 1E-7);
+        assertEquals(22.0, data.get(1, 1), 1E-7);
+        assertEquals(23.0, data.get(2, 1), 1E-7);
+    }
+
+    @Test(expected = Test.None.class)
+    public void testMatrixMarketDense() throws Exception {
+        System.out.println("market dense");
+        Matrix data = (Matrix) Matrix.market(smile.util.Paths.getTestData("matrix/m_10_01.mm"));
+        assertEquals(false, data.isSymmetric());
+        assertEquals(10, data.nrows());
+        assertEquals(1, data.ncols());
+        assertEquals(0.193523, data.get(0, 0), 1E-7);
+        assertEquals(0.200518, data.get(1, 0), 1E-7);
+        assertEquals(0.625800, data.get(2, 0), 1E-7);
+        assertEquals(0.585233, data.get(3, 0), 1E-7);
+        assertEquals(0.127585, data.get(9, 0), 1E-7);
+    }
 }
