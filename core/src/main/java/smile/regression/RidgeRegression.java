f89562474cc5793d2ab10dd7f3e8227299026b3d
Haifeng Li <haifeng.hli@gmail.com>
2020-08-09 22:24:19 -0400

    generalized ridge regression

--- core/src/main/java/smile/regression/RidgeRegression.java
+++ core/src/main/java/smile/regression/RidgeRegression.java
@@ -27,27 +27,28 @@ import smile.math.blas.UPLO;
 import smile.math.matrix.Matrix;
 
 /**
- * Ridge Regression. Coefficient estimates for multiple linear regression models rely on
- * the independence of the model terms. When terms are correlated and
- * the columns of the design matrix X have an approximate linear dependence,
- * the matrix <code>X'X</code> becomes close to singular. As a result, the least-squares estimate
- * becomes highly sensitive to random errors in the observed response <code>Y</code>,
- * producing a large variance.
+ * Ridge Regression. Coefficient estimates for multiple linear regression
+ * models rely on the independence of the model terms. When terms are
+ * correlated and the columns of the design matrix X have an approximate
+ * linear dependence, the matrix <code>X'X</code> becomes close to singular.
+ * As a result, the least-squares estimate becomes highly sensitive to random
+ * errors in the observed response <code>Y</code>, producing a large variance.
  * <p>
  * Ridge regression is one method to address these issues. In ridge regression,
- * the matrix <code>X'X</code> is perturbed so as to make its determinant appreciably
- * different from 0.
+ * the matrix <code>X'X</code> is perturbed so as to make its determinant
+ * appreciably different from 0.
  * <p>
  * Ridge regression is a kind of Tikhonov regularization, which is the most
  * commonly used method of regularization of ill-posed problems.
  * Ridge regression shrinks the regression coefficients by imposing a penalty
  * on their size. By allowing a small amount of bias in the estimates, more
- * reasonable coefficients may often be obtained. Often, small amounts of bias lead to
- * dramatic reductions in the variance of the estimated model coefficients.</p>
+ * reasonable coefficients may often be obtained. Often, small amounts of bias
+ * lead to dramatic reductions in the variance of the estimated model
+ * coefficients.
  * <p>
- * Another interpretation of ridge regression is available through Bayesian estimation.
- * In this setting the belief that weight should be small is coded into a prior
- * distribution.
+ * Another interpretation of ridge regression is available through Bayesian
+ * estimation. In this setting the belief that weight should be small is
+ * coded into a prior distribution.
  * <p>
  * The penalty term is unfair if the predictor variables are not on the
  * same scale. Therefore, if we know that the variables are not measured
@@ -55,10 +56,10 @@ import smile.math.matrix.Matrix;
  * variance 1), and then we perform ridge regression.
  * <p>
  * When including an intercept term in the regression, we usually leave
- * this coefficient unpenalized. Otherwise we could add some constant amount
- * to the vector <code>y</code>, and this would not result in the same solution.
- * If we center the columns of <code>X</code>, then the intercept estimate
- * ends up just being the mean of <code>y</code>.
+ * this coefficient unpenalized. Otherwise we could add some constant
+ * amount to the vector <code>y</code>, and this would not result in
+ * the same solution. If we center the columns of <code>X</code>, then
+ * the intercept estimate ends up just being the mean of <code>y</code>.
  * <p>
  * Ridge regression doesn’t set coefficients exactly to zero unless
  * <code>&lambda; = &infin;</code>, in which case they’re all zero.
@@ -106,10 +107,32 @@ public class RidgeRegression {
      *               Choosing an appropriate value of lambda is important, and also difficult.
      */
     public static LinearModel fit(Formula formula, DataFrame data, double lambda) {
-        if (lambda < 0.0) {
-            throw new IllegalArgumentException("Invalid shrinkage/regularization parameter lambda = " + lambda);
-        }
+        int n = data.size();
+        double[] weights = new double[n];
+        Arrays.fill(weights, 1.0);
+        return fit(formula, data, weights, new double[]{lambda}, new double[]{0.0});
+    }
 
+    /**
+     * Fits a generalized ridge regression model that minimizes a
+     * weighted least squares criterion augmented with a
+     * generalized ridge penalty:
+     * <pre><code>
+     *     (Y - X'*beta)' * W * (Y - X'*beta) + (beta - beta0)' * lambda * (beta - beta0)
+     * </code></pre>
+     *
+     * @param formula a symbolic description of the model to be fitted.
+     * @param data the data frame of the explanatory and response variables.
+     *             NO NEED to include a constant column of 1s for bias.
+     * @param weights sample weights.
+     * @param lambda the shrinkage/regularization parameter. Large lambda
+     *               means more shrinkage. Choosing an appropriate value of
+     *               lambda is important, and also difficult. Its length may
+     *               be 1 so that its value is applied to all variables.
+     * @param beta0 generalized ridge penalty target. Its length may
+     *              be 1 so that its value is applied to all variables.
+     */
+    public static LinearModel fit(Formula formula, DataFrame data, double[] weights, double[] lambda, double[] beta0) {
         formula = formula.expand(data.schema());
         StructType schema = formula.bind(data.schema());
 
@@ -121,6 +144,38 @@ public class RidgeRegression {
 
         if (n <= p) {
             throw new IllegalArgumentException(String.format("The input matrix is not over determined: %d rows, %d columns", n, p));
+        }
+
+        if (weights.length != n) {
+            throw new IllegalArgumentException(String.format("Invalid weights vector size: %d != %d", weights.length, n));
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (weights[i] <= 0.0) {
+                throw new IllegalArgumentException(String.format("Invalid weights[%d] = %f", i, weights[i]));
+            }
+        }
+
+        if (lambda.length == 1) {
+            double shrinkage = lambda[0];
+            lambda = new double[p];
+            Arrays.fill(lambda, shrinkage);
+        } else if (lambda.length != p) {
+            throw new IllegalArgumentException(String.format("Invalid lambda vector size: %d != %d", lambda.length, p));
+        }
+
+        for (int i = 0; i < p; i++) {
+            if (lambda[i] < 0.0) {
+                throw new IllegalArgumentException(String.format("Invalid lambda[%d] = %f", i, lambda[i]));
+            }
+        }
+
+        if (beta0.length == 1) {
+            double beta = beta0[0];
+            beta0 = new double[p];
+            Arrays.fill(beta0, beta);
+        } else if (beta0.length != p) {
+            throw new IllegalArgumentException(String.format("Invalid beta0 vector size: %d != %d", beta0.length, p));
         }
 
         LinearModel model = new LinearModel();
@@ -138,12 +193,22 @@ public class RidgeRegression {
         }
 
         Matrix scaledX = X.scale(center, scale);
-        double[] scaledY = scaledX.tv(y);
+        Matrix XtW = new Matrix(p, n);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                XtW.set(i, j, weights[j] * scaledX.get(j, i));
+            }
+        }
 
-        Matrix XtX = scaledX.ata();
+        double[] scaledY = XtW.mv(y);
+        for (int i = 0; i < p; i++) {
+            scaledY[i] += lambda[i] * beta0[i];
+        }
+
+        Matrix XtX = XtW.mm(scaledX);
         XtX.uplo(UPLO.LOWER);
         for (int i = 0; i < p; i++) {
-            XtX.add(i, i, lambda);
+            XtX.add(i, i, lambda[i]);
         }
         Matrix.Cholesky cholesky = XtX.cholesky();
 
