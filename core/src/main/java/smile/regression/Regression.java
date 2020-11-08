8111781fd83fa0517694c86461679fd35ca10342
Haifeng Li <haifeng.hli@gmail.com>
2020-11-08 17:16:57 -0500

    Regression.Metric

--- core/src/main/java/smile/regression/Regression.java
+++ core/src/main/java/smile/regression/Regression.java
@@ -18,7 +18,9 @@
 package smile.regression;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.function.ToDoubleFunction;
+import smile.math.MathEx;
 
 /**
  * Regression analysis includes any techniques for modeling and analyzing
@@ -56,5 +58,90 @@ public interface Regression<T> extends ToDoubleFunction<T>, Serializable {
     @Override
     default double applyAsDouble(T x) {
         return predict(x);
+    }
+
+    /**
+     * Regression metrics.
+     */
+    class Metric {
+        /**
+         * The values of responsible variable.
+         */
+        public final double[] y;
+        /**
+         * The fitted values.
+         */
+        public final double[] fittedValues;
+        /**
+         * The residuals, that is response minus fitted values.
+         */
+        public final double[] residuals;
+        /**
+         * Residual sum of squares.
+         */
+        public final double RSS;
+        /**
+         * R<sup>2</sup> coefficient of determination. An R<sup>2</sup>
+         * of 1.0 indicates that the regression line perfectly fits the data.
+         */
+        public final double RSquared;
+        /**
+         * Mean squared error.
+         */
+        public final double MSE;
+        /**
+         * Root mean squared error.
+         */
+        public final double RMSE;
+        /**
+         * Mean absolute deviation error.
+         */
+        public final double MAD;
+
+        /** Constructor. */
+        private Metric(double[] y, double[] fittedValues) {
+            this.y = y;
+            this.fittedValues = fittedValues;
+
+            int n = y.length;
+            residuals = new double[n];
+
+            double mad = 0.0;
+            double rss = 0.0;
+            double TSS = 0.0;
+            double ybar = MathEx.mean(y);
+            for (int i = 0; i < n; i++) {
+                double r = y[i] - fittedValues[i];
+                residuals[i] = r;
+                rss += r * r;
+                mad += Math.abs(r);
+
+                double t = y[i] - ybar;
+                TSS += t * t;
+            }
+
+            RSS = rss;
+            RSquared = 1.0 - RSS / TSS;
+
+            MSE = Math.sqrt(rss/n);
+            RMSE = Math.sqrt(MSE);
+
+            MAD = mad / n;
+        }
+    }
+
+    /** Returns the regression metrics. */
+    default Metric metric(T[] x, double[] y) {
+        int n = x.length;
+        double[] fittedValues = new double[n];
+        for (int i = 0; i < n; i++) {
+            fittedValues[i] = predict(x[i]);
+        }
+        return new Metric(y, fittedValues);
+    }
+
+    /** Returns the regression metrics. */
+    default Metric metric(double[] y, double[] fittedValues) {
+        return new Metric(y, fittedValues);
     }
 }
