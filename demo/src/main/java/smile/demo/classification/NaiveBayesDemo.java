4356cfe6a2f1079d69cb436839e2f8eed4c2af8d
Haifeng Li <haifeng.hli@gmail.com>
2020-12-06 23:35:54 -0500

    update header

--- demo/src/main/java/smile/demo/classification/NaiveBayesDemo.java
+++ demo/src/main/java/smile/demo/classification/NaiveBayesDemo.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2010-2020 Haifeng Li. All rights reserved.
  *
  * Smile is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
- ******************************************************************************/
+ */
 
 package smile.demo.classification;
 
