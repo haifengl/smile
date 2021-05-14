16f4a0ef2927f99359bce0074a804d2ba61a47ed
Haifeng Li <haifeng.hli@gmail.com>
2021-05-13 23:49:47 -0400

    support cross validation and ensemble

--- shell/src/main/scala/smile/model/Model.scala
+++ shell/src/main/scala/smile/model/Model.scala
@@ -18,12 +18,13 @@
 package smile.model
 
 import java.util.Properties
-import smile.base.rbf.RBF
+import scala.jdk.CollectionConverters._
 import smile.data.DataFrame
 import smile.data.formula.Formula
 import smile.data.`type`.StructType
 import smile.classification._
 import smile.regression.{DataFrameRegression, ElasticNet, GaussianProcessRegression, LASSO, OLS, RegressionTree, RidgeRegression, SVR}
+import smile.validation.{ClassificationMetrics, CrossValidation}
 
 /**
   * The machine learning model applicable on a data frame.
@@ -43,11 +44,17 @@ sealed trait DataFrameModel {
   * @param schema the schema of input data (without response variable).
   * @param formula the model formula.
   * @param classifier the classification model.
+  * @param train the training metrics.
+  * @param validation the cross-validation metrics.
+  * @param test the test metrics.
   */
 case class ClassificationModel(override val algorithm: String,
                                override val schema: StructType,
                                override val formula: Formula,
-                               classifier: DataFrameClassifier) extends DataFrameModel
+                               classifier: DataFrameClassifier,
+                               train: ClassificationMetrics,
+                               validation: Option[ClassificationMetrics],
+                               test: Option[ClassificationMetrics]) extends DataFrameModel
 
 object ClassificationModel {
   /**
@@ -56,10 +63,50 @@ object ClassificationModel {
     * @param formula the model formula.
     * @param data the training data.
     * @param prop the hyperparameters.
+    * @param kfold k-fold cross validation if kfold > 1.
+    * @param round the number of repeated cross validation.
+    * @param ensemble create the ensemble of cross validation models if true.
+    * @param test the test data.
     * @return the classification model.
     */
-  def apply(algorithm: String, formula: Formula, data: DataFrame, prop: Properties): ClassificationModel = {
-    val model: DataFrameClassifier = algorithm match {
+  def apply(algorithm: String, formula: Formula, data: DataFrame, prop: Properties,
+            kfold: Int = 1, round: Int = 1, ensemble: Boolean = false,
+            test: Option[DataFrame] = None): ClassificationModel = {
+    val start = System.nanoTime()
+    val (model, validationMetrics) = if (kfold < 2) {
+      val model = fit(algorithm, formula, data, prop)
+      (model, None)
+    } else {
+      val cv = CrossValidation.stratify(round, kfold, formula, data, (f, d) => fit(algorithm, f, d, prop))
+      val models = cv.rounds.asScala.map(round => round.model).toArray
+      val model = if (ensemble)
+        DataFrameClassifier.ensemble(models: _*)
+      else
+        fit(algorithm, formula, data, prop)
+
+      (model, Some(cv.avg))
+    }
+
+    val fitTime = (System.nanoTime() - start) / 1E6
+    val trainMetrics = ClassificationMetrics.of(fitTime, model, formula, data)
+    val testMetrics = test.map(ClassificationMetrics.of(model, formula, _))
+
+    val y = formula.response().variables()
+    val predictors = data.schema().fields().filter(field => !y.contains(field.name))
+    val schema = new StructType(predictors: _*)
+    ClassificationModel(algorithm, schema, formula, model, trainMetrics, validationMetrics, testMetrics)
+  }
+
+  /**
+    * Trains a classification model.
+    * @param algorithm the algorithm name.
+    * @param formula the model formula.
+    * @param data the training data.
+    * @param prop the hyperparameters.
+    * @return the classification model.
+    */
+  def fit(algorithm: String, formula: Formula, data: DataFrame, prop: Properties): DataFrameClassifier = {
+    algorithm match {
       case "random.forest" =>
         smile.classification.RandomForest.fit(formula, data, prop)
       case "gradient.boost" =>
@@ -87,11 +134,6 @@ object ClassificationModel {
       case _ =>
         throw new IllegalArgumentException("Unsupported algorithm: " + algorithm)
     }
-
-    val y = formula.response().variables()
-    val predictors = data.schema().fields().filter(field => !y.contains(field.name))
-    val schema = new StructType(predictors: _*)
-    ClassificationModel(algorithm, schema, formula, model)
   }
 }
 
--- shell/src/main/scala/smile/shell/Train.scala
+++ shell/src/main/scala/smile/shell/Train.scala
@@ -39,6 +39,8 @@ import smile.validation._
   * @param model the model file path.
   * @param classification true if the problem type is classification.
   * @param kfold k-fold cross validation.
+  * @param round the number of rounds of repeated cross validation.
+  * @param ensemble the flag to create the ensemble of cross validation models.
   * @param params the hyperparameter key-value pairs.
   */
 case class TrainConfig(algorithm: String = "",
@@ -48,7 +50,9 @@ case class TrainConfig(algorithm: String = "",
                        format: String = "",
                        model: String = "",
                        classification: Boolean = true,
-                       kfold: Int = 0,
+                       kfold: Int = 1,
+                       round: Int = 1,
+                       ensemble: Boolean = false,
                        params: Map[String, String] = Map())
 
 /**
@@ -69,11 +73,11 @@ object Train {
         val props = getHyperparameters(config.algorithm, config.params)
 
         if (config.classification) {
-          val model = ClassificationModel(config.algorithm, formula, data, props)
-          if (test.isDefined) {
-            val metrics = ClassificationMetrics.of(model.classifier, formula, test.get)
-            println(s"Validation metrics: ${metrics}")
-          }
+          val model = ClassificationModel(config.algorithm, formula, data, props, config.kfold, config.round, config.ensemble, test)
+          println(s"Training metrics: ${model.train}")
+          model.validation.map(metrics => println(s"Validation metrics: ${metrics}"))
+          model.test.map(metrics => println(s"Test metrics: ${metrics}"))
+
           smile.write(model, config.model)
         } else {
           val model = RegressionModel(config.algorithm, formula, data, props)
@@ -137,6 +141,14 @@ object Train {
           .optional()
           .action((x, c) => c.copy(kfold = x))
           .text("The k-fold cross validation"),
+        opt[Int]("round")
+          .optional()
+          .action((x, c) => c.copy(round = x))
+          .text("The number of rounds of repeated cross validation"),
+        opt[Unit]("ensemble")
+          .optional()
+          .action((_, c) => c.copy(ensemble = true))
+          .text("Ensemble of cross validation models."),
         opt[Map[String, String]]("params")
           .valueName("k1=v1,k2=v2...")
           .action((x, c) => c.copy(params = x))
@@ -146,121 +158,6 @@ object Train {
 
     OParser.parse(parser, args, TrainConfig())
     // If arguments be bad, the error message would have been displayed.
-  }
-
-  /**
-    * Trains a data frame classifier model.
-    * @param formula the model formula.
-    * @param data the training data.
-    * @param props the hyperparameters.
-    * @param test the optional validation data.
-    * @param config the training configuration.
-    * @return the model.
-    */
-  def trainDataFrameClassifier(formula: Formula, data: DataFrame, props: Properties,
-                               test: Option[DataFrame], config: TrainConfig)
-                              (trainer: (Formula, DataFrame, Properties) => DataFrameClassifier): ClassificationModel = {
-    val start = System.nanoTime()
-    val model = trainer(formula, data, props)
-    val fitTime = (System.nanoTime() - start) / 1E6
-
-    if (config.kfold > 1) {
-      val metrics = cv.classification(config.kfold, formula, data) { (formula, data) =>
-        trainer(formula, data, props)
-      }
-      println(s"${config.kfold}-fold cross validation metrics: ${metrics}")
-    } else {
-      val metrics = ClassificationMetrics.of(fitTime, model, formula, data)
-      println(s"Training metrics: ${metrics}")
-    }
-
-    if (test.isDefined) {
-      val metrics = ClassificationMetrics.of(model, formula, test.get)
-      println(s"Validation metrics: ${metrics}")
-    }
-
-    val y = formula.response().variables()
-    val predictors = data.schema().fields().filter(field => !y.contains(field.name)).toList
-    val schema = new StructType(predictors: _*)
-    val numClasses = MathEx.unique(formula.y(data).toIntArray()).length
-    null //ClassificationModel(config.algorithm, formula, schema, numClasses, model)
-  }
-
-  /**
-    * Trains a data frame regression model.
-    * @param formula the model formula.
-    * @param data the training data.
-    * @param prop the hyperparameters.
-    * @param test the optional validation data.
-    * @param config the training configuration.
-    * @return the model.
-    */
-  def trainDataFrameRegression(formula: Formula, data: DataFrame, prop: Properties,
-                               test: Option[DataFrame], config: TrainConfig)
-                              (trainer: (Formula, DataFrame, Properties) => DataFrameRegression): RegressionModel = {
-    val start = System.nanoTime()
-    val model = trainer(formula, data, prop)
-    val fitTime = (System.nanoTime() - start) / 1E6
-
-    if (config.kfold > 1) {
-      val metrics = cv.regression(config.kfold, formula, data) { (formula, data) =>
-        trainer(formula, data, prop)
-      }
-      println(s"${config.kfold}-fold cross validation metrics: ${metrics}")
-    } else {
-      val metrics = RegressionMetrics.of(fitTime, model, formula, data)
-      println(s"Training metrics: ${metrics}")
-    }
-
-    if (test.isDefined) {
-      val metrics = RegressionMetrics.of(model, formula, test.get)
-      println(s"Validation metrics: ${metrics}")
-    }
-
-    null //RegressionModel(config.algorithm, formula, null, model)
-  }
-
-  /**
-    * Trains a classifier taking vector input.
-    * @param formula the model formula.
-    * @param data the training data.
-    * @param prop the hyperparameters.
-    * @param bias the flag to generate the bias term.
-    * @param encoder the categorical variable encoder.
-    * @param test the optional validation data.
-    * @param config the training configuration.
-    * @return the model.
-    */
-  def trainVectorClassifier(formula: Formula, data: DataFrame, prop: Properties,
-                            bias: Boolean, encoder: CategoricalEncoder,
-                            test: Option[DataFrame], config: TrainConfig)
-                           (trainer: (Array[Array[Double]], Array[Int], Properties) => Classifier[Array[Double]]): ClassificationModel = {
-    val x = formula.x(data).toArray(bias, encoder)
-    val y = formula.y(data).toIntArray()
-
-    val start = System.nanoTime()
-    val model = trainer(x, y, prop)
-    val fitTime = (System.nanoTime() - start) / 1E6
-
-    if (config.kfold > 1) {
-      val metrics = cv.classification(config.kfold, x, y) { (x, y) =>
-        trainer(x, y, prop)
-      }
-      println(s"${config.kfold}-fold cross validation metrics: ${metrics}")
-    } else {
-      val metrics = ClassificationMetrics.of(fitTime, model, x, y)
-      println(s"Training metrics: ${metrics}")
-    }
-
-    if (test.isDefined) {
-      val testx = formula.x(test.get).toArray(bias, encoder)
-      val testy = formula.y(test.get).toIntArray()
-      val metrics = ClassificationMetrics.of(model, testx, testy)
-      println(s"Validation metrics: ${metrics}")
-    }
-
-    val numClasses = MathEx.unique(formula.y(data).toIntArray()).length
-    null //ClassificationModel(config.algorithm, formula, numClasses, model)
   }
 
   /**
