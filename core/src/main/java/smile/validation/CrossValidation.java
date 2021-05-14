44f59991631e3fc5dca72f0b9d21ab4e26db09d0
Haifeng Li <haifeng.hli@gmail.com>
2021-05-14 10:40:34 -0400

    always shuffle with cross validation

--- core/src/main/java/smile/validation/CrossValidation.java
+++ core/src/main/java/smile/validation/CrossValidation.java
@@ -53,17 +53,6 @@ public interface CrossValidation {
      * @return k-fold data splits.
      */
     static Bag[] of(int n, int k) {
-        return of(n, k, true);
-    }
-
-    /**
-     * Creates a k-fold cross validation.
-     * @param n the number of samples.
-     * @param k the number of rounds of cross validation.
-     * @param shuffle whether to shuffle samples before splitting.
-     * @return k-fold data splits.
-     */
-    static Bag[] of(int n, int k, boolean shuffle) {
         if (n < 0) {
             throw new IllegalArgumentException("Invalid sample size: " + n);
         }
@@ -73,11 +62,7 @@ public interface CrossValidation {
         }
 
         Bag[] bags = new Bag[k];
-
-        int[] index = IntStream.range(0, n).toArray();
-        if (shuffle){
-            MathEx.permutate(index);
-        }
+        int[] index = MathEx.permutate(n);
 
         int chunk = n / k;
         for (int i = 0; i < k; i++) {
