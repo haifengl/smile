6c4f1f9acd9721e97de9ae614a1690ce41014865
Haifeng Li <haifeng.hli@gmail.com>
2021-06-15 23:00:26 -0400

    update test

--- mkl/src/test/java/smile/math/matrix/BiconjugateGradientTest.java
+++ mkl/src/test/java/smile/math/matrix/BiconjugateGradientTest.java
@@ -69,9 +69,9 @@ public class BiconjugateGradientTest {
     public void testSolveMatrix() {
         System.out.println("naive matrix");
 
-        Matrix naive = Matrix.of(A);
+        Matrix matrix = Matrix.of(A);
         double[] result = new double[3];
-        BiconjugateGradient.solve(naive, b, result);
+        matrix.solve(b, result);
 
         assertEquals(result.length, x.length);
         for (int i = 0; i < x.length; i++) {
@@ -88,7 +88,7 @@ public class BiconjugateGradientTest {
         SparseMatrix sparse = new SparseMatrix(A, 1E-8);
 
         double[] result = new double[3];
-        BiconjugateGradient.solve(sparse, b, result);
+        sparse.solve(b, result);
 
         assertEquals(result.length, x.length);
         for (int i = 0; i < x.length; i++) {
