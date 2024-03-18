d5215515ed2b598430af0f6741c55825716446f9
Haifeng Li <haifeng.hli@gmail.com>
2024-03-18 14:16:10 -0400

    add VegaTest

---
+++ plot/src/test/java/smile/plot/vega/VegaTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2010-2021 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package smile.plot.vega;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Haifeng Li
+ */
+public class VegaTest {
+
+    public VegaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testBar() throws Exception {
+        System.out.println("bar");
+
+        View bar = new View().
+                title("Bar Plot").
+                description("A simple bar chart with embedded data.").
+                widthStep(30).
+                json("""
+      [
+        {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+        {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+        {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+      ]""");
+
+        bar.mark("bar");
+        Field x = bar.encoding("x", "a");
+        x.type("ordinal");
+        Field y = bar.encoding("y", "b");
+        y.type("quantitative");
+        // x(field = "a", `type` = "ordinal", axis = JsObject("labelAngel" -> JsInt(0))).
+
+        bar.show();
+    }
+}
