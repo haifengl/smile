2db9ccb7f2f6dbd2abffc90c11c05a2de66b3ee3
Haifeng Li <haifeng.hli@gmail.com>
2024-03-18 10:34:10 -0400

    implement ViewLayoutComposition

--- plot/src/main/java/smile/plot/vega/ViewLayoutComposition.java
+++ plot/src/main/java/smile/plot/vega/ViewLayoutComposition.java
@@ -16,67 +16,101 @@
  */
 package smile.plot.vega;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
- * all view layout composition (facet, concat, and repeat) can have the
+ * All view layout composition (facet, concat, and repeat) can have the
  * following layout properties: align, bounds, center, spacing.
  *
  * @author Haifeng Li
  */
 public class ViewLayoutComposition extends ViewComposition {
-    public ViewLayoutComposition(String layout, VegaLite... views) {
-        ArrayNode array = mapper.createArrayNode();
-        for (VegaLite view : views) {
-            array.add(view.spec);
-        }
-        spec.set(layout, array);
-    }
-
-    public ViewLayoutComposition(VegaLite view) {
-        spec.set("spec", view.spec);
+    /**
+     * Hides the constructor so that users cannot create the instances directly.
+     */
+    ViewLayoutComposition() {
     }
 
     /**
-     * Creates a view for each entry in an array of fields. This operator
-     * generates multiple plots like facet. However, unlike facet it allows
-     * full replication of a data set in each view.
-     *
-     * @param fields The fields that should be used for each entry.
+     * Sets the alignment to apply to grid rows and columns. The supported string
+     * values are "all" (the default), "each", and "none".
+     * <p>
+     * For "none", a flow layout will be used, in which adjacent subviews
+     * are simply placed one after the other.
+     * <p>
+     * For "each", subviews will be aligned into a clean grid structure,
+     * but each row or column may be of variable size.
+     * <p>
+     * For "all", subviews will be aligned and each row or column will be
+     * sized identically based on the maximum observed size. String values
+     * for this property will be applied to both grid rows and columns.
      */
-    public ViewLayoutComposition repeat(String... fields) {
-        ArrayNode layer = mapper.createArrayNode();
-        for (String field : fields) {
-            layer.add(field);
-        }
-        ObjectNode repeat = mapper.createObjectNode();
-        repeat.set("layer", layer);
-        spec.set("repeat", repeat);
+    public ViewLayoutComposition align(String align) {
+        spec.put("align", align);
         return this;
     }
 
     /**
-     * Creates a view for each entry in an array of fields. This operator
-     * generates multiple plots like facet. However, unlike facet it allows
-     * full replication of a data set in each view.
-     *
-     * @param row    An array of fields to be repeated vertically.
-     * @param column An array of fields to be repeated horizontally.
+     * Sets different alignments for rows and columns.
      */
-    public ViewLayoutComposition repeat(String[] row, String[] column) {
-        ObjectNode repeat = mapper.createObjectNode();
-        ArrayNode rows = mapper.createArrayNode();
-        ArrayNode columns = mapper.createArrayNode();
-        for (String field : row) {
-            rows.add(field);
-        }
-        for (String field : column) {
-            columns.add(field);
-        }
-        repeat.set("row", rows);
-        repeat.set("column", columns);
-        spec.set("repeat", repeat);
+    public ViewLayoutComposition align(String row, String column) {
+        ObjectNode node = spec.putObject("align");
+        node.put("row", row);
+        node.put("column", column);
+        return this;
+    }
+
+    /**
+     * Sets the bounds calculation method to use for determining the extent
+     * of a sub-plot. One of full (the default) or flush.
+     * <p>
+     * If set to full, the entire calculated bounds (including axes,
+     * title, and legend) will be used.
+     * <p>
+     * If set to flush, only the specified width and height values
+     * for the sub-view will be used. The flush setting can be useful
+     * when attempting to place sub-plots without axes or legends into
+     * a uniform grid structure.
+     */
+    public ViewLayoutComposition bounds(String bounds) {
+        spec.put("bounds", bounds);
+        return this;
+    }
+
+    /**
+     * Sets if subviews should be centered relative to
+     * their respective rows or columns.
+     */
+    public ViewLayoutComposition center(boolean flag) {
+        spec.put("center", flag);
+        return this;
+    }
+
+    /**
+     * Sets different spacing values for rows and columns.
+     */
+    public ViewLayoutComposition center(int row, int column) {
+        ObjectNode node = spec.putObject("center");
+        node.put("row", row);
+        node.put("column", column);
+        return this;
+    }
+
+    /**
+     * Sets the spacing in pixels between sub-views of the composition operator.
+     */
+    public ViewLayoutComposition spacing(int size) {
+        spec.put("spacing", size);
+        return this;
+    }
+
+    /**
+     * Sets different spacing values for rows and columns.
+     */
+    public ViewLayoutComposition spacing(int row, int column) {
+        ObjectNode node = spec.putObject("spacing");
+        node.put("row", row);
+        node.put("column", column);
         return this;
     }
 }
