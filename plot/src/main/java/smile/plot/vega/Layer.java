efcf0f6e943d89e47e064a7000f6f0b52d5df1af
Haifeng Li <haifeng.hli@gmail.com>
2024-03-19 15:43:55 -0400

    make Layer extends View

--- plot/src/main/java/smile/plot/vega/Layer.java
+++ plot/src/main/java/smile/plot/vega/Layer.java
@@ -17,14 +17,13 @@
 package smile.plot.vega;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * To superimpose one chart on top of another.
  *
  * @author Haifeng Li
  */
-public class Layer extends ViewComposition {
+public class Layer extends View implements ViewComposition {
     /**
      * Constructor.
      *
@@ -42,187 +41,93 @@ public class Layer extends ViewComposition {
         spec.set("layer", layer);
     }
 
-    /**
-     * Sets the width of a plot with a continuous x-field,
-     * or the fixed width of a plot a discrete x-field or no x-field.
-     */
+    @Override
     public Layer width(int width) {
-        spec.put("width", width);
+        super.width(width);
         return this;
     }
 
-    /**
-     * Sets the height of a plot with a continuous y-field,
-     * or the fixed height of a plot a discrete y-field or no y-field.
-     */
+    @Override
     public Layer height(int height) {
-        spec.put("height", height);
+        super.height(height);
         return this;
     }
 
-    /**
-     * To enable responsive sizing on width.
-     * @param width it should be set to "container".
-     */
+    @Override
     public Layer width(String width) {
-        assert width == "container" : "Invalid width: " + width;
-        spec.put("width", "container");
+        super.width(width);
         return this;
     }
 
-    /**
-     * To enable responsive sizing on height.
-     * @param height it should be set to "container".
-     */
+    @Override
     public Layer height(String height) {
-        assert height == "container" : "Invalid height: " + height;
-        spec.put("height", "container");
+        super.height(height);
         return this;
     }
 
-    /**
-     * For a discrete x-field, sets the width per discrete step.
-     */
+    @Override
     public Layer widthStep(int step) {
-        ObjectNode width = spec.putObject("width");
-        width.put("step", step);
+        super.widthStep(step);
         return this;
     }
 
-    /**
-     * For a discrete y-field, sets the height per discrete step..
-     */
+    @Override
     public Layer heightStep(int step) {
-        ObjectNode height = spec.putObject("height");
-        height.put("step", step);
+        super.heightStep(step);
         return this;
     }
 
-    /**
-     * Returns the view background's fill and stroke object.
-     */
-    public Background background() {
-        return new Background(spec.has("view") ? (ObjectNode) spec.get("view") : spec.putObject("view"));
-    }
-
-    /**
-     * Returns the defining properties of geographic projection, which will be
-     * applied to shape path for "geoshape" marks and to latitude and
-     * "longitude" channels for other marks.
-     * @param type The cartographic projection to use.
-     * @link https://vega.github.io/vega-lite/docs/projection.html#projection-types
-     */
-    public Projection projection(String type) {
-        ObjectNode node = spec.putObject("projection");
-        node.put("type", type);
-        return new Projection(node);
-    }
-
-    /**
-     * Returns the field object for encoding a channel.
-     * @param channel Vega-Lite supports the following groups of encoding channels:
-     *   - Position Channels: x, y, x2, y2, xError, yError, xError2, yError2
-     *   - Position Offset Channels: xOffset, yOffset
-     *   - Polar Position Channels: theta, theta2, radius, radius2
-     *   - Geographic Position Channels: longitude, latitude, longitude2, latitude2
-     *   - Mark Property Channels: angle, color (and fill / stroke), opacity, fillOpacity, strokeOpacity, shape, size, strokeDash, strokeWidth
-     *   - Text and Tooltip Channels: text, tooltip
-     *   - Hyperlink Channel: href
-     *   - Description Channel: description
-     *   - Level of Detail Channel: detail
-     *   - Key Channel: key
-     *   - Order Channel: order
-     *   - Facet Channels: facet, row, column
-     * @param field A string defining the name of the field from which to pull
-     *             a data value. Dots (.) and brackets ([ and ]) can be used to
-     *             access nested objects (e.g., "field": "foo.bar" and
-     *             "field": "foo['bar']"). If field names contain dots or
-     *             brackets but are not nested, you can use \\ to escape dots
-     *             and brackets (e.g., "a\\.b" and "a\\[0\\]").
-     * @return the field object for encoding the channel.
-     */
-    public Field encoding(String channel, String field) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("field", field);
-        return new Field(node);
-    }
-
-    /**
-     * Sets an encoded constant visual value.
-     * @param channel the encoding channel.
-     * @param value the constant visual value.
-     * @return this object.
-     */
-    public Layer encodingValue(String channel, int value) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("value", value);
+    @Override
+    public Layer encodeValue(String channel, int value) {
+        super.encodeValue(channel, value);
         return this;
     }
 
-    /**
-     * Sets an encoded constant visual value.
-     * @param channel the encoding channel.
-     * @param value the constant visual value.
-     * @return this object.
-     */
-    public Layer encodingValue(String channel, double value) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("value", value);
+    @Override
+    public Layer encodeValue(String channel, double value) {
+        super.encodeValue(channel, value);
         return this;
     }
 
-    /**
-     * Sets an encoded constant visual value.
-     * @param channel the encoding channel.
-     * @param value the constant visual value.
-     * @return this object.
-     */
-    public Layer encodingValue(String channel, String value) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("value", value);
+    @Override
+    public Layer encodeValue(String channel, String value) {
+        super.encodeValue(channel, value);
         return this;
     }
 
-    /**
-     * Sets a constant data value encoded via a scale.
-     * @param channel the encoding channel.
-     * @param datum the constant data value.
-     * @return this object.
-     */
-    public Layer encodingDatum(String channel, int datum) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("datum", datum);
+    @Override
+    public Layer encodeDatum(String channel, int datum) {
+        super.encodeDatum(channel, datum);
         return this;
     }
 
-    /**
-     * Sets a constant data value encoded via a scale.
-     * @param channel the encoding channel.
-     * @param datum the constant data value.
-     * @return this object.
-     */
-    public Layer encodingDatum(String channel, double datum) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("datum", datum);
+    @Override
+    public Layer encodeDatum(String channel, double datum) {
+        super.encodeDatum(channel, datum);
         return this;
     }
 
-    /**
-     * Sets a constant data value encoded via a scale.
-     * @param channel the encoding channel.
-     * @param datum the constant data value.
-     * @return this object.
-     */
-    public Layer encodingDatum(String channel, String datum) {
-        ObjectNode encoding = encoding();
-        ObjectNode node = encoding.putObject(channel);
-        node.put("datum", datum);
+    @Override
+    public Layer encodeDatum(String channel, String datum) {
+        super.encodeDatum(channel, datum);
+        return this;
+    }
+
+    @Override
+    public Layer resolveScale(String channel, String resolution) {
+        ViewComposition.super.resolveScale(channel, resolution);
+        return this;
+    }
+
+    @Override
+    public Layer resolveAxis(String channel, String resolution) {
+        ViewComposition.super.resolveAxis(channel, resolution);
+        return this;
+    }
+
+    @Override
+    public Layer resolveLegend(String channel, String resolution) {
+        ViewComposition.super.resolveLegend(channel, resolution);
         return this;
     }
 }
