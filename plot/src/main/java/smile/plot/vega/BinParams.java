07b18ea2d5323bec131cd8370d55209f1924d8a7
Haifeng Li <haifeng.hli@gmail.com>
2024-03-19 23:57:38 -0400

    add BinParams and Predicate

---
+++ plot/src/main/java/smile/plot/vega/BinParams.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2010-2021 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package smile.plot.vega;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * To test a data point in a filter transform or a test property in conditional
+ * encoding, a predicate definition of the following forms must be specified:
+ *
+ * - a Vega expression string, where datum can be used to refer to the current
+ *   data object. For example, datum.b2 > 60 would test if the value in the
+ *   field b2 for each data point is over 60.
+ *
+ * - one of the field predicates: equal, lt, lte, gt, gte, range, oneOf, or valid.
+ *
+ * - a parameter predicate, which defines the names of a selection that the data
+ *   point should belong to (or a logical composition of selections).
+ *
+ * - a logical composition of (1), (2), or (3).
+ *
+ * @author Haifeng Li
+ */
+public class BinParams {
+    /** The BinParams specification. */
+    final ObjectNode spec = VegaLite.mapper.createObjectNode();
+
+    /** Constructor. */
+    public BinParams() {
+
+    }
+
+    /**
+     * Sets the value in the binned domain at which to anchor the bins,
+     * shifting the bin boundaries if necessary to ensure that a boundary
+     * aligns with the anchor value.
+     *
+     * @param value the anchor value.
+     * @return this object.
+     */
+    public BinParams anchor(double value) {
+        spec.put("anchor", value);
+        return this;
+    }
+
+    /**
+     * Sets the number base to use for automatic bin determination
+     * (default is base 10).
+     *
+     * @param base the base number.
+     * @return this object.
+     */
+    public BinParams base(int base) {
+        spec.put("base", base);
+        return this;
+    }
+
+    /**
+     * Sets the scale factors indicating allowable subdivisions. The default
+     * value is [5, 2], which indicates that for base 10 numbers (the default
+     * base), the method may consider dividing bin sizes by 5 and/or 2.
+     * For example, for an initial step size of 10, the method can check
+     * if bin sizes of 2 (= 10/5), 5 (= 10/2), or 1 (= 10/(5*2)) might also
+     * satisfy the given constraints.
+     *
+     * @param factors the scale factors
+     * @return this object.
+     */
+    public BinParams divide(int... factors) {
+        ArrayNode node = spec.putArray("divide");
+        for (var factor : factors) {
+            node.add(factor);
+        }
+        return this;
+    }
+
+    /**
+     * Sets the range of desired bin values
+     *
+     * @param min the lower bound of desired bin values.
+     * @param max the upper bound of desired bin values.
+     * @return this object.
+     */
+    public BinParams extent(double min, double max) {
+        spec.putArray("extent").add(min).add(max);
+        return this;
+    }
+
+    /**
+     * Sets the maximum number of bins.
+     *
+     * @param bins the maximum number of bins.
+     * @return this object.
+     */
+    public BinParams maxBins(int bins) {
+        spec.put("maxbins", bins);
+        return this;
+    }
+
+    /**
+     * Sets the minimum allowable step size (particularly useful for
+     * integer values).
+     *
+     * @param size the minimum allowable step size.
+     * @return this object.
+     */
+    public BinParams minStep(double size) {
+        spec.put("minstep", size);
+        return this;
+    }
+
+    /**
+     * Sets the exact step size between bins.
+     *
+     * @param size the exact step size between bins.
+     * @return this object.
+     */
+    public BinParams step(double size) {
+        spec.put("step", size);
+        return this;
+    }
+
+    /**
+     * Sets an array of allowable step sizes to choose from.
+     *
+     * @param steps an array of allowable step sizes to choose from.
+     * @return this object.
+     */
+    public BinParams steps(double... steps) {
+        ArrayNode node = spec.putArray("steps");
+        for (var step : steps) {
+            node.add(step);
+        }
+        return this;
+    }
+
+    /**
+     * If true, attempts to make the bin boundaries use human-friendly
+     * boundaries, such as multiples of ten.
+     *
+     * @param flag If true, attempts to make the bin boundaries use
+     *            human-friendly boundaries, such as multiples of ten.
+     * @return this object.
+     */
+    public BinParams nice(int flag) {
+        spec.put("nice", flag);
+        return this;
+    }
+}
---
+++ plot/src/main/java/smile/plot/vega/Predicate.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 2010-2021 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package smile.plot.vega;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * To test a data point in a filter transform or a test property in conditional
+ * encoding, a predicate definition of the following forms must be specified:
+ *
+ * - a Vega expression string, where datum can be used to refer to the current
+ *   data object. For example, datum.b2 > 60 would test if the value in the
+ *   field b2 for each data point is over 60.
+ *
+ * - one of the field predicates: equal, lt, lte, gt, gte, range, oneOf, or valid.
+ *
+ * - a parameter predicate, which defines the names of a selection that the data
+ *   point should belong to (or a logical composition of selections).
+ *
+ * - a logical composition of (1), (2), or (3).
+ *
+ * @author Haifeng Li
+ */
+public class Predicate {
+    /** The predicate specification object. */
+    final ObjectNode spec;
+    /** The Vega expression string. */
+    final String expr;
+
+    /**
+     * Constructor.
+     * @param expr a Vega expression string.
+     */
+    public Predicate(String expr) {
+        this.spec = null;
+        this.expr = expr;
+    }
+
+    /**
+     * Constructor of parameter predicate.
+     * For example, with {"param": "brush"}, only data values that fall within
+     * the selection named brush will remain in the dataset. Notice, by default,
+     * empty selections are considered to contain all data values. We can toggle
+     * this behavior by setting the optional empty property true on the predicate.
+     *
+     * @param param Filter using a parameter name.
+     * @param empty For selection parameters, the predicate of empty selections
+     *             returns true by default. Override this behavior, by setting
+     *             this property false.
+     */
+    public Predicate(String param, boolean empty) {
+        this.expr = null;
+        this.spec = VegaLite.mapper.createObjectNode();
+        spec.put("param", param).put("empty", empty);
+    }
+
+    /**
+     * Constructor.
+     * @param spec the predicate specification object.
+     */
+    Predicate(ObjectNode spec) {
+        this.spec = spec;
+        this.expr = null;
+    }
+
+    /**
+     * Sets the time unit for a temporal field. Vega-Lite supports the following time units:
+     *
+     * "year" - Gregorian calendar years.
+     * "quarter" - Three-month intervals, starting in one of January, April, July, and October.
+     * "month" - Calendar months (January, February, etc.).
+     * "date" - Calendar day of the month (January 1, January 2, etc.).
+     * "week" - Sunday-based weeks. Days before the first Sunday of the year are considered to be
+     *          in week 0, the first Sunday of the year is the start of week 1, the second Sunday
+     *          week 2, etc.
+     * "day" - Day of the week (Sunday, Monday, etc.).
+     * "dayofyear" - Day of the year (1, 2, …, 365, etc.).
+     * "hours" - Hours of the day (12:00am, 1:00am, etc.).
+     * "minutes" - Minutes in an hour (12:00, 12:01, etc.).
+     * "seconds" - Seconds in a minute (12:00:00, 12:00:01, etc.).
+     * "milliseconds" - Milliseconds in a second.
+     *
+     * @param timeUnit  Time unit.
+     * @return this object.
+     */
+    public Predicate timeUnit(String timeUnit) {
+        assert spec != null && spec.has("field") : "Not a Field Predicate";
+        spec.put("timeUnit", timeUnit);
+        return this;
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param op equal, lt (less than), lte (less than or equal),
+     *          gt (greater than), or gte(greater than or equal).
+     * @param value the value to compare.
+     * @return a field predicate.
+     */
+    public static Predicate of(String field, String op, boolean value) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field).put(op, value);
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param op equal, lt (less than), lte (less than or equal),
+     *          gt (greater than), or gte(greater than or equal).
+     * @param value the value to compare.
+     * @return a field predicate.
+     */
+    public static Predicate of(String field, String op, double value) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field).put(op, value);
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param op equal, lt (less than), lte (less than or equal),
+     *          gt (greater than), or gte(greater than or equal).
+     * @param value the value to compare.
+     * @return a field predicate.
+     */
+    public static Predicate of(String field, String op, String value) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field).put(op, value);
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param min inclusive minimum values for a field value of a data
+     *           item to be included in the filtered data.
+     * @param max inclusive maximum values for a field value of a data
+     *           item to be included in the filtered data.
+     * @param max the value to compare.
+     * @return a field predicate.
+     */
+    public static Predicate range(String field, double min, double max) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field);
+        node.putArray("range").add(min).add(max);
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param values a set of values that the field's value should be a member
+     *              of, for a data item included in the filtered data.
+     * @return a field predicate.
+     */
+    public static Predicate oneOf(String field, double... values) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field);
+        ArrayNode array = node.putArray("oneOf");
+        for (var value : values) {
+            array.add(value);
+        }
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field in the data point satisfies certain conditions.
+     *
+     * @param field the field to be tested.
+     * @param values a set of values that the field's value should be a member
+     *              of, for a data item included in the filtered data.
+     * @return a field predicate.
+     */
+    public static Predicate oneOf(String field, String... values) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field);
+        ArrayNode array = node.putArray("oneOf");
+        for (var value : values) {
+            array.add(value);
+        }
+        return new Predicate(node);
+    }
+
+    /**
+     * Test if a field is valid, meaning it is neither null nor NaN.
+     *
+     * @param field the field to be tested.
+     * @return a field predicate.
+     */
+    public static Predicate valid(String field) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        node.put("field", field).put("valid", true);
+        return new Predicate(node);
+    }
+
+    /**
+     * Logical and composition to combine predicates.
+     * @param predicates the predicates.
+     * @return and predicate.
+     */
+    public static Predicate and(Predicate... predicates) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        ArrayNode array = node.putArray("and");
+        for (var predicate : predicates) {
+            if (predicate.expr != null) {
+                array.add(predicate.expr);
+            } else {
+                array.add(predicate.spec);
+            }
+        }
+        return new Predicate(node);
+    }
+
+    /**
+     * Logical or composition to combine predicates.
+     * @param predicates the predicates.
+     * @return or predicate.
+     */
+    public static Predicate or(Predicate... predicates) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        ArrayNode array = node.putArray("or");
+        for (var predicate : predicates) {
+            if (predicate.expr != null) {
+                array.add(predicate.expr);
+            } else {
+                array.add(predicate.spec);
+            }
+        }
+        return new Predicate(node);
+    }
+
+    /**
+     * Logical not operation.
+     * @param predicate the predicate.
+     * @return not predicate.
+     */
+    public static Predicate or(Predicate predicate) {
+        ObjectNode node = VegaLite.mapper.createObjectNode();
+        if (predicate.expr != null) {
+            node.put("not", predicate.expr);
+        } else {
+            node.set("not", predicate.spec);
+        }
+        return new Predicate(node);
+    }
+}
