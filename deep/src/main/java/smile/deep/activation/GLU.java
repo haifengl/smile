2d72b4bd6f56f58a9a08c8ea238fb68ce8e5b30b
Haifeng Li <haifeng.hli@gmail.com>
2024-04-03 18:54:38 -0400

    add GLU

---
+++ deep/src/main/java/smile/deep/activation/GLU.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2010-2021 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package smile.deep.activation;
+
+import org.bytedeco.pytorch.Tensor;
+import org.bytedeco.pytorch.global.torch;
+
+/**
+ * Gated Linear Unit activation function.
+ *
+ * @author Haifeng Li
+ */
+public class GLU extends ActivationFunction {
+    /**
+     * Constructor.
+     */
+    public GLU() {
+        super("GLU", false);
+    }
+
+    @Override
+    public Tensor apply(Tensor x) {
+        return torch.glu(x);
+    }
+}
