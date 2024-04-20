c14e8a9b28db7eaf1c113bcf980b35f28f659669
Haifeng Li <haifeng.hli@gmail.com>
2024-04-20 11:54:20 -0400

    initialize in no_grad() mode

--- deep/src/main/java/smile/vision/EfficientNet.java
+++ deep/src/main/java/smile/vision/EfficientNet.java
@@ -112,36 +112,39 @@ public class EfficientNet extends LayerBlock {
         add("avgpool", avgpool);
         add("classifier", classifier);
 
-        // Initialization
-        var modules = asTorch().modules();
-        for (int i = 0; i < modules.size(); i++) {
-            var module = modules.get(i);
-            var name = module.name().getString();
-            switch (name) {
-                case "torch::nn::Conv2dImpl":
-                    var conv2d = module.asConv2d();
-                    torch.kaiming_normal_(conv2d.weight(), 0.0, new FanModeType(new kFanOut()), new Nonlinearity(new kLeakyReLU()));
-                    var bias = conv2d.bias();
-                    if (bias.defined()) {
-                        bias.detach().zero_();
-                    }
-                    break;
-                case "torch::nn::BatchNorm2dImpl":
-                    var batchNorm2d = module.asBatchNorm2d();
-                    torch.ones_(batchNorm2d.weight());
-                    batchNorm2d.bias().detach().zero_();
-                    break;
-                case "torch::nn::GroupNormImpl":
-                    var groupNorm = module.asGroupNorm();
-                    torch.ones_(groupNorm.weight());
-                    groupNorm.bias().detach().zero_();
-                    break;
-                case "torch::nn::LinearImpl":
-                    var linear = module.asLinear();
-                    double range = 1.0 / Math.sqrt(linear.options().out_features().get());
-                    torch.uniform_(linear.weight(), -range, range);
-                    torch.zeros_(linear.bias());
-                    break;
+        // Initialization should run in torch.no_grad() mode and
+        // will not be taken into account by autograd.
+        try (var guard = Tensor.noGradGuard()) {
+            var modules = asTorch().modules();
+            for (int i = 0; i < modules.size(); i++) {
+                var module = modules.get(i);
+                var name = module.name().getString();
+                switch (name) {
+                    case "torch::nn::Conv2dImpl":
+                        var conv2d = module.asConv2d();
+                        torch.kaiming_normal_(conv2d.weight(), 0.0, new FanModeType(new kFanOut()), new Nonlinearity(new kLeakyReLU()));
+                        var bias = conv2d.bias();
+                        if (bias.defined()) {
+                            bias.zero_();
+                        }
+                        break;
+                    case "torch::nn::BatchNorm2dImpl":
+                        var batchNorm2d = module.asBatchNorm2d();
+                        torch.ones_(batchNorm2d.weight());
+                        batchNorm2d.bias().zero_();
+                        break;
+                    case "torch::nn::GroupNormImpl":
+                        var groupNorm = module.asGroupNorm();
+                        torch.ones_(groupNorm.weight());
+                        groupNorm.bias().zero_();
+                        break;
+                    case "torch::nn::LinearImpl":
+                        var linear = module.asLinear();
+                        double range = 1.0 / Math.sqrt(linear.options().out_features().get());
+                        torch.uniform_(linear.weight(), -range, range);
+                        torch.zeros_(linear.bias());
+                        break;
+                }
             }
         }
     }
