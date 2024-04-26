834d79dce96cf52444ac0167cd28d5382185617d
Haifeng Li <haifeng.hli@gmail.com>
2024-04-26 19:35:13 -0400

    LocalDate::from(TemporalAccessor)

--- base/src/main/java/smile/data/vector/StringVectorImpl.java
+++ base/src/main/java/smile/data/vector/StringVectorImpl.java
@@ -64,7 +64,7 @@ class StringVectorImpl extends VectorImpl<String> implements StringVector {
 
     @Override
     public Vector<LocalDate> toDate(DateTimeFormatter format) {
-        LocalDate[] dates = stream().map(format::parse).toArray(LocalDate[]::new);
+        LocalDate[] dates = stream().map(format::parse).map(LocalDate::from).toArray(LocalDate[]::new);
         return new VectorImpl<>(name(), DataTypes.DateType, dates);
     }
 
@@ -75,7 +75,7 @@ class StringVectorImpl extends VectorImpl<String> implements StringVector {
 
     @Override
     public Vector<LocalTime> toTime(DateTimeFormatter format) {
-        LocalTime[] dates = stream().map(format::parse).toArray(LocalTime[]::new);
+        LocalTime[] dates = stream().map(format::parse).map(LocalTime::from).toArray(LocalTime[]::new);
         return new VectorImpl<>(name(), DataTypes.TimeType, dates);
     }
 
@@ -86,7 +86,7 @@ class StringVectorImpl extends VectorImpl<String> implements StringVector {
 
     @Override
     public Vector<LocalDateTime> toDateTime(DateTimeFormatter format) {
-        LocalDateTime[] dates = stream().map(format::parse).toArray(LocalDateTime[]::new);
+        LocalDateTime[] dates = stream().map(format::parse).map(LocalDateTime::from).toArray(LocalDateTime[]::new);
         return new VectorImpl<>(name(), DataTypes.DateTimeType, dates);
     }
 
