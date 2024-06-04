0dd7a4d7ebbf37c02868619ac1ca1f5b74636559
Haifeng Li <haifeng.hli@gmail.com>
2024-06-04 11:08:09 -0400

    add RotaryPositionalEncodingTest

---
+++ deep/src/test/java/smile/llm/RotaryPositionalEncodingTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2010-2024 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package smile.llm;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Haifeng Li
+ */
+public class RotaryPositionalEncodingTest {
+
+    public RotaryPositionalEncodingTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void test() {
+        var cis = RotaryPositionalEncoding.computeFreqCis(128, 4096, 500000.0);
+
+        long[] shape = { 4096, 64 };
+        assertArrayEquals(shape, cis.shape());
+
+        var real = cis.viewAsReal();
+        // real has shape [4096, 64, 2]
+        assertEquals(1.0f, real.getFloat(0, 0, 0), 1E-4);
+        assertEquals(0.0f, real.getFloat(0, 0, 1), 1E-4);
+        assertEquals(1.0f, real.getFloat(0, 1, 0), 1E-4);
+        assertEquals(0.0f, real.getFloat(0, 1, 1), 1E-4);
+        assertEquals(0.5403f, real.getFloat(1, 0, 0), 1E-4);
+        assertEquals(0.8415f, real.getFloat(1, 0, 1), 1E-4);
+        assertEquals(0.6861f, real.getFloat(1, 1, 0), 1E-4);
+        assertEquals(0.7275f, real.getFloat(1, 1, 1), 1E-4);
+    }
+}
