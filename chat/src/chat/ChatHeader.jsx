68776b3e7ab772ce4d4ce3ae58676319ffd4a2b7
Karl Li <kkli@umich.edu>
2024-06-21 23:46:32 -0400

    MessageHeader -> ChatHeader

--- chat/src/chat/ChatHeader.jsx
+++ chat/src/chat/ChatHeader.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './ChatHeader.css'
 
-export default function MessageHeader({
+export default function ChatHeader({
     children
 }) {
     return (
-        <div className="message-header">
+        <div className="chat-header">
             <div className="inner-container">
                 <div className="heading-container">
                     <div className="chat-title">{children}</div>
