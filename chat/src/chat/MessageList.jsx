1a2f16dd86b90d4f6cecd8adf0dfe75791d25894
Karl Li <kkli@umich.edu>
2024-06-22 17:07:38 -0400

    always scroll to bottom when messages or showTypingIndicator change

--- chat/src/chat/MessageList.jsx
+++ chat/src/chat/MessageList.jsx
@@ -11,55 +11,8 @@ export default function MessageList({
     typingIndicatorContent,
     theme = '#6ea9d7',
 }) {
-    const [messagesWasEmpty, setMessagesWasEmpty] = useState(true)
-    const containerRef = useRef()
-    const bottomBufferRef = useRef()
-    const scrollContainerRef = useRef()
-
-    const detectBottom = (ref) => {
-        if (ref.current) {
-            const threshold = 100;
-            return ref.current.scrollHeight - ref.current.scrollTop <= ref.current.clientHeight + threshold;
-        }
-        return false;
-    }
-
-    useEffect(() => {
-        // scroll to the bottom when the scroll view is first rendered
-        // and messages have rendered.
-        if (bottomBufferRef.current && scrollContainerRef.current && !messagesWasEmpty) {
-            scrollToBottom()
-        }
-
-    }, [messagesWasEmpty, bottomBufferRef.current, scrollContainerRef.current])
-
-
-    useEffect(() => {
-        if (!messages) {
-            setMessagesWasEmpty(true)
-        }
-
-        if (messages) {
-            if (messagesWasEmpty) {
-                // scroll to bottom if the messages object was previously empty
-                // e.g., when the first page of messages arrives.
-                // However, don't scroll to bottom if the next page of messages arrives.
-                setMessagesWasEmpty(false)
-                scrollToBottom()
-            }
-
-            // scroll to bottom when closer to the bottom of the scroll bar
-            // and a new message arrives.
-            if (detectBottom(scrollContainerRef)) {
-                scrollToBottom()
-            }
-
-        }
-    }, [messages])
-
-    useEffect(() => {
-        scrollToBottom()
-    }, [showTypingIndicator])
+    const bottomBufferRef = useRef();
+    const scrollContainerRef = useRef();
 
     const scrollToBottom = async () => {
         if (bottomBufferRef.current && scrollContainerRef.current) {
@@ -80,17 +33,19 @@ export default function MessageList({
         }
     }
 
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages, showTypingIndicator])
+
     return (
-        <div className="message-list" ref={containerRef}>
+        <div className="message-list">
             <div className="scroll-background-container">
                 <div className="scroll-background"/>
             </div>
 
             <div style={{ "height" : "100%" }}>
-                <div className="scroll-container"
-                    ref={scrollContainerRef}
-                >
-                    {messages && scrollContainerRef.current && bottomBufferRef.current && messages.map(({ user, text, createdAt }, index) => {
+                <div className="scroll-container" ref={scrollContainerRef}>
+                    {messages && messages.map(({ user, text, createdAt }, index) => {
                         if (user.id == (userId && userId.toLowerCase())) {
                             return <OutgoingMessage key={index}
                                     user={user}
