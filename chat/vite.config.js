6c3dcdc5c4aa4d30cd477da52b97cdccbe67e858
Karl Li <kkli@umich.edu>
2024-06-30 11:24:10 -0400

    add svgr plugin

--- chat/vite.config.js
+++ chat/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,5 @@ export default defineConfig({
   build: {
     outDir: '../serve/src/universal/chat'
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 })
