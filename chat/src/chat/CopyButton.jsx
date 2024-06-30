778ce38c0070f4588e53134c17f1df1afa58838b
Karl Li <kkli@umich.edu>
2024-06-30 11:05:22 -0400

    CopyButton without FontAwesome

--- chat/src/chat/CopyButton.jsx
+++ chat/src/chat/CopyButton.jsx
@@ -1,28 +1,41 @@
-import React from "react";
-import 'font-awesome/css/font-awesome.min.css'
+import React from 'react';
+import Check from '../assets/check-square.svg?react';
+import Clone from '../assets/clone.svg?react';
 import './CopyButton.css';
 
 export default function CopyButton({ children }) {
-    const [copyOk, setCopyOk] = React.useState(false);
-
-    const iconColor = copyOk ? '#0af20a' : '#ddd';
-    const icon = navigator.clipboard ? (copyOk ? 'fa-check-square' : 'fa-clone') : 'fa-fw';
+    const [clicked, setClicked] = React.useState(false);
 
     const handleClick = (e) => {
         // clipboard requires a secure origin — either HTTPS or localhost
-        if (navigator.clipboard) {
+        if (!clicked && navigator.clipboard) {
             navigator.clipboard.writeText(children.props.children);
 
-            setCopyOk(true);
+            setClicked(true);
             setTimeout(() => {
-                setCopyOk(false);
+                setClicked(false);
             }, 500);
         }
     }
 
+    /* css based, working but throw warnings. src must be specified, other <img>
+       won't show. Meanwhile, it must be a wrong path so that <img> shows only
+       the background that already draws the icon as background mask.
+    const icon = clicked ? 'check' : 'clone';
     return (
         <div className="copy-btn">
-            <i className={`fa ${icon}`} onClick={handleClick} style={{color: iconColor}} />
+            {navigator.clipboard &&
+                <img src='../assets/clone.svg' className={icon} onClick={handleClick} />
+            }
+        </div>
+    )
+    */
+    const button = clicked ? <Check className='icon' fill='#0af20a' />
+        : <Clone className='icon' onClick={handleClick} fill='#ddd' />;
+
+    return (
+        <div className="copy-btn">
+            {navigator.clipboard && button}
         </div>
     )
 }
