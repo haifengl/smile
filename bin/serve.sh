fb2f17ae5e6aae3ddfd870bc9c9cbd182302a637
Haifeng Li <haifeng.hli@gmail.com>
2024-07-15 10:41:20 -0400

    take script arguments

--- bin/serve.sh
+++ bin/serve.sh
@@ -5,4 +5,4 @@ npm install
 npm run build
 cd ../
 sbt serve/stage
-serve/target/universal/stage/bin/smile-serve -J-Dakka.http.server.interface=0.0.0.0 --model model/Llama-3-8B-Instruct --tokenizer model/Llama-3-8B-Instruct/tokenizer.model
+serve/target/universal/stage/bin/smile-serve "$@"
