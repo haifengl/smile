774871b107a2dc62f38a2d604412e1376a237820
Haifeng Li <haifeng.hli@gmail.com>
2025-01-02 00:39:30 -0500

    update copyright year

--- core/src/main/java/smile/base/cart/Loss.java
+++ core/src/main/java/smile/base/cart/Loss.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 Haifeng Li. All rights reserved.
+ * Copyright (c) 2010-2025 Haifeng Li. All rights reserved.
  *
  * Smile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package smile.base.cart;
 
 import smile.math.MathEx;
