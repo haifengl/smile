d1bc82c1b8adc5342e52caf57aa9d967514efad0
Haifeng Li <haifeng.hli@gmail.com>
2025-09-16 22:20:49 -0400

    add Cholesky

---
+++ base/src/main/java/smile/tensor/Cholesky.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2010-2025 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smile. If not, see <https://www.gnu.org/licenses/>.
+ */
+package smile.tensor;
+
+import smile.linalg.LAPACK;
+
+/**
+ * The Cholesky decomposition of a symmetric, positive-definite matrix.
+ * When it is applicable, the Cholesky decomposition is roughly twice as
+ * efficient as the LU decomposition for solving systems of linear equations.
+ * <p>
+ * The Cholesky decomposition is mainly used for the numerical solution of
+ * linear equations. The Cholesky decomposition is also commonly used in
+ * the Monte Carlo method for simulating systems with multiple correlated
+ * variables: The matrix of inter-variable correlations is decomposed,
+ * to give the lower-triangular L. Applying this to a vector of uncorrelated
+ * simulated shocks, u, produces a shock vector Lu with the covariance
+ * properties of the system being modeled.
+ * <p>
+ * Unscented Kalman filters commonly use the Cholesky decomposition to choose
+ * a set of so-called sigma points. The Kalman filter tracks the average
+ * state of a system as a vector x of length n and covariance as an n-by-n
+ * matrix P. The matrix P is always positive semi-definite, and can be
+ * decomposed into L*L'. The columns of L can be added and subtracted from
+ * the mean x to form a set of 2n vectors called sigma points. These sigma
+ * points completely capture the mean and covariance of the system state.
+ *
+ * @author Haifeng Li
+ */
+public record Cholesky(DenseMatrix lu) {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Cholesky.class);
+
+    /**
+     * Constructor.
+     * @param lu the lower/upper triangular part of matrix contains the Cholesky
+     *           factorization.
+     */
+    public Cholesky {
+        if (lu.nrow() != lu.ncol()) {
+            throw new UnsupportedOperationException("Cholesky constructor on a non-square matrix");
+        }
+    }
+
+    /**
+     * Returns the matrix determinant.
+     * @return the matrix determinant.
+     */
+    public double det() {
+        int n = lu.n;
+        double d = 1.0;
+        for (int i = 0; i < n; i++) {
+            d *= lu.get(i, i);
+        }
+
+        return d * d;
+    }
+
+    /**
+     * Returns the log of matrix determinant.
+     * @return the log of matrix determinant.
+     */
+    public double logdet() {
+        int n = lu.n;
+        double d = 0.0;
+        for (int i = 0; i < n; i++) {
+            d += Math.log(lu.get(i, i));
+        }
+
+        return 2.0 * d;
+    }
+
+    /**
+     * Returns the inverse of matrix.
+     * @return the inverse of matrix.
+     */
+    public DenseMatrix inverse() {
+        DenseMatrix inv = lu.eye(lu.n);
+        solve(inv);
+        return inv;
+    }
+
+    /**
+     * Solves the linear system A * x = b.
+     * @param b the right hand side of linear systems. On output, b will
+     *          be overwritten with the solution vector.
+     */
+    public void solve(double[] b) {
+        solve(Vector.column(b));
+    }
+
+    /**
+     * Solves the linear system A * X = B.
+     * @param B the right hand side of linear systems. On output, B will
+     *          be overwritten with the solution matrix.
+     */
+    public void solve(DenseMatrix B) {
+        if (B.scalarType() != lu.scalarType()) {
+            throw new IllegalArgumentException("Incompatible ScalarType: " + B.scalarType() + " != " + lu.scalarType());
+        }
+        if (B.m != lu.m) {
+            throw new IllegalArgumentException(String.format("Row dimensions do not agree: A is %d x %d, but B is %d x %d", lu.m, lu.n, B.m, B.n));
+        }
+
+        int info = LAPACK.potrs(lu.layout(), lu.uplo, lu.n, B.n, lu.memory, lu.ld, B.memory, B.ld);
+        if (info != 0) {
+            logger.error("LAPACK POTRS error code: {}", info);
+            throw new ArithmeticException("LAPACK POTRS error code: " + info);
+        }
+    }
+}
