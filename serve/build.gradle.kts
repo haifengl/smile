6a87494c071e2a43871d2778fd9ba007b525e6db
Haifeng Li <haifeng.hli@gmail.com>
2025-12-30 16:45:46 -0500

    pass jvmArgs to quarkusDev

--- serve/build.gradle.kts
+++ serve/build.gradle.kts
@@ -18,19 +18,20 @@ dependencies {
     testImplementation("io.quarkus:quarkus-junit5")
 }
 
-quarkus {
-    buildForkOptions {
-        jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED",
-            "--add-opens", "java.base/java.nio=ALL-UNNAMED",
-            "---enable-native-access", "ALL-UNNAMED")
-    }
-}
-
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
-    jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED",
+            "--add-opens", "java.base/java.nio=ALL-UNNAMED",
+            "--enable-native-access", "ALL-UNNAMED")
+}
+tasks.quarkusDev {
+    jvmArgs = listOf(
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "--add-opens", "java.base/java.nio=ALL-UNNAMED",
+        "--enable-native-access", "ALL-UNNAMED")
 }
 tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
 }
 tasks.withType<Javadoc> {
