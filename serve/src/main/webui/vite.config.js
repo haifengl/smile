a2c17cbf6492aa0316545e0292dd33ee7fdf27d3
Haifeng Li <haifeng.hli@gmail.com>
2026-01-15 18:53:25 -0500

    move chat to serve/src/main/webui

--- serve/src/main/webui/vite.config.js
+++ serve/src/main/webui/vite.config.js
@@ -20,7 +20,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../serve/src/main/resources/META-INF/resources',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
