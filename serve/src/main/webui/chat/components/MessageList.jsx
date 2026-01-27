77845ece2c2f2b8563194c734f0e6b332d0e4310
Haifeng Li <haifeng.hli@gmail.com>
2026-01-26 21:29:52 -0500

    cosmetic edit

--- serve/src/main/webui/chat/components/MessageList.jsx
+++ serve/src/main/webui/chat/components/MessageList.jsx
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2010-2025 Haifeng Li. All rights reserved.
  *
- * Smile is free software: you can redistribute it and/or modify it
+ * SMILE is free software: you can redistribute it and/or modify it
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Smile is distributed in the hope that it will be useful, but
+ * SMILE is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Smile. If not, see <https://www.gnu.org/licenses/>.
+ * along with SMILE. If not, see <https://www.gnu.org/licenses/>.
  */
 import React, { useEffect, useRef, useState } from 'react'
 import IncomingMessage from './IncomingMessage'
