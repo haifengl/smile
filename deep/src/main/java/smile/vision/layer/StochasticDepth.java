3e0e3d6adbb0f84c5c636badb046d31dbe9e65d1
Haifeng Li <haifeng.hli@gmail.com>
2026-02-25 16:20:37 -0500

    copyright 2026

--- deep/src/main/java/smile/vision/layer/StochasticDepth.java
+++ deep/src/main/java/smile/vision/layer/StochasticDepth.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2024 Haifeng Li. All rights reserved.
+ * Copyright (c) 2010-2026 Haifeng Li. All rights reserved.
  *
  * SMILE is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
