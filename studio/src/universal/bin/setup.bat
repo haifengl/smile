31a576ceb53966aa52fcac5564afc2702fd2b4a7
Haifeng Li <haifeng.hli@gmail.com>
2026-04-03 22:05:44 -0400

    install ty

--- studio/src/universal/bin/setup.bat
+++ studio/src/universal/bin/setup.bat
@@ -1,7 +1,7 @@
 @REM smile setup script
 
 @echo off
-REM install ripgrep
+ECHO Installing ripgrep...
 winget install -e --id BurntSushi.ripgrep.MSVC
 
 SET "APP_HOME=%~dp0\\.."
@@ -14,8 +14,9 @@ IF NOT EXIST "%VENV_DIR%\\Scripts\\activate.bat" (
         ECHO Failed to create the virtual environment. Ensure Python is installed and added to PATH.
     ) ELSE (
         ECHO Virtual environment created successfully.
-        ECHO Activating the virtual environment...
         CALL "%VENV_DIR%\\Scripts\\activate.bat"
         pip install -r %APP_HOME%\\conf\\requirements.txt
+        pip install uv
+        uv tool install ty@latest
     )
 )
