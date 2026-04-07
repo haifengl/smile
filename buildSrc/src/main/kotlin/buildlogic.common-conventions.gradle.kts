630ac617efe7fa54072ccfe405c3a16792e268ca
Haifeng Li <haifeng.hli@gmail.com>
2026-04-06 20:37:51 -0400

    update PATH for OpenBLAS

--- buildSrc/src/main/kotlin/buildlogic.common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.common-conventions.gradle.kts
@@ -13,6 +13,22 @@ tasks.withType<Test>().all {
     jvmArgs("-Xmx6G", "-XX:+UseG1GC", "-XX:MaxMetaspaceSize=1024M", "-Xss4M")
     jvmArgs("--add-opens=java.base/java.nio=ALL-UNNAMED",)
     jvmArgs("--enable-native-access=ALL-UNNAMED")
+
+    val osName = System.getProperty("os.name").lowercase()
+    val libPath = file("${rootDir.path}/studio/src/universal/bin").absolutePath
+    if (osName.contains("windows")) {
+        // On Windows, DLLs are found via the PATH
+        val currentPath = System.getenv("PATH") ?: ""
+        environment("PATH", "$libPath;$currentPath")
+    } else if (osName.contains("mac")) {
+        // On macOS, shared libraries (.dylib) use DYLD_LIBRARY_PATH
+        val currentDyldPath = System.getenv("DYLD_LIBRARY_PATH") ?: ""
+        environment("DYLD_LIBRARY_PATH", "$libPath:$currentDyldPath")
+    } else {
+        // On Linux, shared libraries (.so) use LD_LIBRARY_PATH
+        val currentLdPath = System.getenv("LD_LIBRARY_PATH") ?: ""
+        environment("LD_LIBRARY_PATH", "$libPath:$currentLdPath")
+    }
 }
 
 tasks.withType<Jar>().all {
