608884a288c1eeba96b283ba213c10277937a913
Haifeng Li <haifeng.hli@gmail.com>
2026-04-16 18:05:31 -0400

    move Trie to smile.util package

--- base/src/main/java/smile/util/Trie.java
+++ base/src/main/java/smile/util/Trie.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SMILE. If not, see <https://www.gnu.org/licenses/>.
  */
-package smile.nlp;
+package smile.util;
 
 import java.util.HashMap;
 import java.util.LinkedList;
