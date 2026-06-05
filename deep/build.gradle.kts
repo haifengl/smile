d5ca7bbcf58fed94688abfbef03fd820d88f3f2e
Haifeng Li <haifeng.hli@gmail.com>
2026-06-05 16:30:38 -0400

    fix test compile errors

--- deep/build.gradle.kts
+++ deep/build.gradle.kts
@@ -9,3 +9,8 @@ dependencies {
 tasks.withType<Test>().all {
     environment("PYTORCH_ENABLE_MPS_FALLBACK", "1")
 }
+
+tasks.withType<Javadoc> {
+    // Exclude generated packages from Javadoc generation
+    exclude("smile/torch/**")
+}
