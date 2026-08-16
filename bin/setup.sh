d9e7842b2db38ea08a6da70d96bb9dd68ec9e53d
Haifeng Li <haifeng.hli@gmail.com>
2026-08-16 14:36:25 -0400

    install nccl2, cusparse, etc

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,5 +6,5 @@ sudo apt install -y libopenblas-dev libarpack2
 
 # Install CUDA
 sudo apt update
-sudo apt install -y cuda-toolkit-12-6 libnccl2 libnccl-dev libcusparselt0 libcudnn9-cuda-12 libnvshmem3-cuda-12
+sudo apt install -y cuda-toolkit-13-2 libnccl2 libnccl-dev libcusparselt0 libcudnn9-cuda-13 libnvshmem3-cuda-13
 
